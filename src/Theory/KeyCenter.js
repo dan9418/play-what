e4479f55acb5338a12ae75b36e5a4fee14a65109
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-03-09 20:28:03 -0700

    allow key center strings

--- src/Theory/KeyCenter.js
+++ src/Theory/KeyCenter.js
@@ -1,8 +1,18 @@
+import { TONIC, ACCIDENTAL } from './Constants';
+
 export default class KeyCenter {
     constructor(tonic, accidental, octave) {
-        this.tonic = tonic;
-        this.accidental = accidental;
-        this.octave = octave; 
+        if (typeof arguments[0] === 'string') {
+            const keyString = arguments[0];
+            this.tonic = TONIC[keyString[0]] || TONIC.C;
+            this.accidental = keyString[1] === '#' ? ACCIDENTAL.Sharp : keyString[1] === 'b' ? ACCIDENTAL.Flat : ACCIDENTAL.Natural;
+            this.octave = 4;
+        }
+        else {
+            this.tonic = tonic;
+            this.accidental = accidental;
+            this.octave = octave;
+        }
     }
 
     copy() {
