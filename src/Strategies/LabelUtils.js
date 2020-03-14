2dda832ed490112fc4e4d5e42624e0a5ffe3dcb4
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-03-14 10:11:00 -0700

    Revert label utils format

--- src/Strategies/LabelUtils.js
+++ src/Strategies/LabelUtils.js
@@ -1,6 +1,6 @@
 const EMPTY = '';
 
-const LabelUtils = {
+/*const LabelUtils = {
     none: () => EMPTY,
     name: (note) => {
         if (!note) return EMPTY;
@@ -26,19 +26,88 @@ const LabelUtils = {
         if (!note) return EMPTY;
 
         return note.pitchClass;
-    }
-    ,
+    },
     octave: (note) => {
         if (!note) return EMPTY;
 
         return note.octave;
-    }
-    ,
+    },
     frequency: (note) => {
         if (!note) return EMPTY;
 
         return Math.round(note.frequency);
     }
-}
+}*/
+
+const LabelUtils = {
+    none: {
+        id: 'none',
+        name: 'None',
+        fx: () => EMPTY
+    },
+    name: {
+        id: 'name',
+        name: 'Name',
+        fx: (note) => {
+            if (!note) return EMPTY;
+    
+            return note.name;
+        }
+    },
+    degree: {
+        id: 'degree',
+        name: 'Degree',
+        fx: (note) => {
+            if (!note || !note.interval) return EMPTY;
+    
+            return note.interval.degree;
+        }
+    },
+    interval: {
+        id: 'interval',
+        name: 'Interval',
+        fx: (note) => {
+            if (!note || !note.interval) return EMPTY;
+    
+            return note.interval.id;
+        }
+    },
+    noteIndex: {
+        id: 'noteIndex',
+        name: 'Note Index',
+        fx: (note) => {
+            if (!note) return EMPTY;
+    
+            return note.noteIndex;
+        }
+    },
+    pitchClass: {
+        id: 'pitchClass',
+        name: 'Pitch Class',
+        fx: (note) => {
+            if (!note) return EMPTY;
+    
+            return note.pitchClass;
+        }
+    },
+    octave: {
+        id: 'octave',
+        name: 'Octave',
+        fx: (note) => {
+            if (!note) return EMPTY;
+    
+            return note.octave;
+        }
+    },
+    frequency: {
+        id: 'frequency',
+        name: 'Frequency',
+        fx: (note) => {
+            if (!note) return EMPTY;
+    
+            return Math.round(note.frequency);
+        }
+    }
+};
 
 export default LabelUtils;
