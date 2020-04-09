a5d53c0ec2fc6df7056668c961b3443b9f3aa9a5
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-08 19:43:24 -0700

    export utils

--- src/v2/_module.js
+++ src/v2/_module.js
@@ -1,11 +1,13 @@
 import * as Constants from './Tools/Constants';
 import * as Presets from './Tools/Presets';
 import Theory from './Tools/Theory';
+import Utils from './Tools/Utils';
 import parse from './Tools/ParseEngine';
 
 export default {
     Constants,
     Presets,
     Theory,
+    Utils,
     parse
 };
