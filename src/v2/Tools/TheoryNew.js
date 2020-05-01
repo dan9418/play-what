083ce46ff957262b33f2c064a5eab929615c7188
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-04-30 20:35:23 -0700

    cleanup

--- src/v2/Tools/TheoryNew.js
+++ src/v2/Tools/TheoryNew.js
@@ -1,3 +1,7 @@
+import { DEGREE_MAPPING, MAX_VECTOR } from "./ConstantsNew";
+import Utils from './Utils';
+import * as Presets from "./PresetsNew";
+
 export const getAccidentalString = (offset) => {
     switch (offset) {
         case 0:
@@ -15,9 +19,28 @@ export const getAccidentalString = (offset) => {
     }
 };
 
-export const addVectors = (origin, vector, max) => {
-    return {
-        p: (origin.p + vector.p) % max.p,
-        d: (origin.d + vector.d) % max.d
-    };
+export const addVectors = (a, b, max) => ({
+    p: Utils.moduloSum(a.p, b.p, max.p),
+    d: Utils.moduloSum(a.d, b.d, max.d)
+});
+
+export const addVectorsBatch = (a, vectors, max = MAX_VECTOR) => vectors.map(v => addVectors(a, v, max));
+
+export const moduloVectors = (a, b, max = MAX_VECTOR) => ({
+    p: Utils.modulo(a.p, b.p),
+    d: Utils.modulo(a.d, b.d)
+});
+
+export const getDegree = degree => DEGREE_MAPPING[degree];
+
+export const getNoteName = (note, max = MAX_VECTOR) => {
+    const reduced = moduloVectors(note, max);
+    const degree = DEGREE_MAPPING[reduced.d];
+    return degree.name + getAccidentalString(degree.pitch - note.p);
+}
+
+export const getIntervalName = (interval, max = MAX_VECTOR) => {
+    const reduced = moduloVectors(interval, max);
+    const preset = Presets.INTERVALS.find(i => i.p === reduced.p && i.d === reduced.d);
+    return preset ? preset.name : '?';
 }
