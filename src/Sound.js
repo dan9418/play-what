51a2c75fe960685d65554c9d08ac289f32c6d07a
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-06-13 08:07:25 -0700

    sound

--- src/Sound.js
+++ src/Sound.js
@@ -1,16 +1,25 @@
 import * as Tone from "tone";
 
 const MASTER_OUT = new Tone.Synth().toMaster();
+const MASTER_OUT_POLY = new Tone.PolySynth(12, Tone.Synth).toMaster();
+const SHAPE = 'sine';
+const VOLUME = new Tone.Volume();
+
+MASTER_OUT_POLY.set({
+    oscillator: {
+      type: SHAPE
+    },
+    envelope: {
+      decay: 1,
+      release: 1
+    }
+  });
 
 export default class SoundUtils {
-    static getSynth() {
-        return MASTER_OUT;
-    }
-    static play(frequency, duration = .5) {
+    static playNote(frequency, duration = .5, velocity = .5) {
         MASTER_OUT.triggerAttackRelease(frequency, duration);
     }
-    static playNotes(freqs, duration = .5) {
-        let synth = new Tone.PolySynth(freqs.length, Tone.Synth).toMaster();
-        synth.triggerAttackRelease(freqs, duration);
+    static playNotes(freqs, duration = .5, velocity = .5) {
+        MASTER_OUT_POLY.triggerAttackRelease(freqs, duration);
     }
 }
