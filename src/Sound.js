d1391887a37028a0bcd5d81df83a9116deaf8211
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-06-13 09:17:52 -0700

    fix volume

--- src/Sound.js
+++ src/Sound.js
@@ -1,19 +1,20 @@
 import * as Tone from "tone";
 
-const MASTER_OUT = new Tone.Synth().toMaster();
-const MASTER_OUT_POLY = new Tone.PolySynth(12, Tone.Synth).toMaster();
-const SHAPE = 'sine';
-const VOLUME = new Tone.Volume();
+const VOLUME = new Tone.Volume(-15);
+
+const MASTER_OUT = new Tone.Synth().chain(VOLUME, Tone.Master);
+const MASTER_OUT_POLY = new Tone.PolySynth(12, Tone.Synth).chain(VOLUME, Tone.Master);
+const SHAPE = 'sine'
 
 MASTER_OUT_POLY.set({
     oscillator: {
-      type: SHAPE
+        type: SHAPE
     },
     envelope: {
-      decay: 1,
-      release: 1
+        decay: 1,
+        release: 1
     }
-  });
+});
 
 export default class SoundUtils {
     static playNote(frequency, duration = .5, velocity = .5) {
