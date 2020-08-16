f3e2751bbaadae8de0bcb2ff4013fc5d51335410
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-16 12:04:31 -0700

    new viewer api

--- src/Viewer.js
+++ src/Viewer.js
@@ -1,11 +1,19 @@
 import Viewers from 'play-what-react-viewers';
 
-export const presets = {
-    fretboard: Viewers.Fretboard.Viewer,
-    keyboard: Viewers.Keyboard.Viewer
+export const fretboard = {
+    component: Viewers.Fretboard.Viewer,
+    create: args => {
+        return {
+            component: Viewers.Fretboard.Viewer,
+            props: {
+                fretMap: [[{ children: 'test' }]]
+            }
+        }
+    }
 }
 
 export const createElement = ({ type, children, ...args }) => {
+    if (typeof type !== 'string') throw ('only string types allowed')
     return {
         component: type,
         children,
