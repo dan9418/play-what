f6c506472463e9877224d5e8642a760292c70d03
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 17:04:06 -0700

    get numerals

--- src/modules/Matrix.Scale.js
+++ src/modules/Matrix.Scale.js
@@ -1,5 +1,19 @@
 import Preset from './Matrix.Scale.presets';
+import * as Utils from './Utils';
+
+const getNumerals = (scale) => {
+  const numerals = [];
+  for (let i = 0; i < scale.length; i++) {
+    const i1 = i;
+    const i3 = Utils.moduloSum(i, 2, scale.length);
+    const i5 = Utils.moduloSum(i, 4, scale.length);
+    const numeral = [scale[i1], scale[i3], scale[i5]];
+    numerals.push(numeral);
+  }
+  return numerals;
+};
 
 export default {
-  Preset
+  Preset,
+  getNumerals
 };
