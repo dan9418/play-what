22cb58b53c8c87ad3a5251b10a59f91e53324c46
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-24 17:19:00 -0700

    fix

--- src/modules/Matrix.Scale.js
+++ src/modules/Matrix.Scale.js
@@ -3,12 +3,13 @@ import * as Utils from './Utils';
 
 const getNumerals = (scale) => {
   const numerals = [];
+  const a = scale[0];
   for (let i = 0; i < scale.length; i++) {
     const i1 = i;
     const i3 = Utils.moduloSum(i, 2, scale.length);
     const i5 = Utils.moduloSum(i, 4, scale.length);
     const numeral = [scale[i1], scale[i3], scale[i5]];
-    numerals.push(numeral);
+    numerals.push({ a, B: numeral });
   }
   return numerals;
 };
