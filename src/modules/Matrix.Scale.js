d430df47dbd4f754c5cf9dc7fa2371247478b136
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-08-25 21:06:02 -0700

    numerals

--- src/modules/Matrix.Scale.js
+++ src/modules/Matrix.Scale.js
@@ -1,20 +1,6 @@
 import Preset from './Matrix.Scale.presets';
 import * as Utils from './Utils';
 
-const getNumerals = (scale) => {
-  const numerals = [];
-  const a = scale[0];
-  for (let i = 0; i < scale.length; i++) {
-    const i1 = i;
-    const i3 = Utils.moduloSum(i, 2, scale.length);
-    const i5 = Utils.moduloSum(i, 4, scale.length);
-    const i7 = Utils.moduloSum(i, 6, scale.length);
-    const numeral = [scale[i1], scale[i3], scale[i5], scale[i7]];
-    numerals.push({ a, B: numeral });
-  }
-  return numerals;
-};
-
 const getMode = ({ scale, degree }) => {
   let mode = [...scale];
   mode = Utils.rotate(mode, degree);
@@ -43,9 +29,30 @@ const getAllModes = ({ scale, keyCenter }) => {
   }));
 };
 
+const getNumeral = ({ scale, keyCenter, degree }) => {
+  const i1 = degree;
+  const i3 = Utils.moduloSum(degree, 2, scale.length);
+  const i5 = Utils.moduloSum(degree, 4, scale.length);
+  const i7 = Utils.moduloSum(degree, 6, scale.length);
+  const numeral = [scale[i1], scale[i3], scale[i5], scale[i7]];
+  return {
+    a: keyCenter,
+    B: numeral
+  };
+};
+
+const getAllNumerals = ({ scale, keyCenter }) => {
+  const numerals = [];
+  for (let i = 0; i < scale.length; i++) {
+    numerals.push(getNumeral({ scale, keyCenter, degree: i }));
+  }
+  return numerals;
+};
+
 export default {
   Preset,
-  getNumerals,
+  getNumeral,
+  getAllNumerals,
   getMode,
   getAllModes
 };
