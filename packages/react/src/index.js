c32b703f1fbfc950933d7b9222fe1698fefaf20b
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-09-11 19:22:06 -0700

    cleanups

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,7 +1,13 @@
-import * as api from './api';
+import Fretboard from './modules/Fretboard/_module';
+import Explorer from './modules/Explorer/_module';
+import Chart from './modules/Chart/_module';
+import List from './modules/List/_module';
 
 export default {
     PW_React: {
-        ...api
+        Fretboard,
+        Explorer,
+        Chart,
+        List
     }
 };
