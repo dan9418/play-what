0ed5da0a3d9a35fa7c45813cd175abfb0039d216
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-09-24 22:07:46 -0700

    cleanups

--- packages/react/src/modules/Docs/ModelRow.jsx
+++ packages/react/src/modules/Docs/ModelRow.jsx
@@ -3,22 +3,42 @@ import Meter from "../viewers/Meter/Meter";
 import { Edit, View} from "../viewers/Preview/Preview";
 import "./Docs.css";
 
+const ViewAsInput = ({ value, setValue }) => {
+	return (
+		<div className='view-as-input'>
+			<div>View As:</div>
+			<div className='view-as-option' onClick={() => setValue('pod')}>Pods</div>
+			<div className='view-as-option' onClick={() => setValue('note')}>Notes</div>
+			<div className='view-as-option' onClick={() => setValue('interval')}>Intervals</div>
+		</div>
+	);
+};
+
 const ModelRow = ({ label, modelType, podType, theoryType, value, setValue, max }) => {
 
+	const [viewAs, setViewAs] = useState(podType);
 	const [isEditing, setIsEditing] = useState(false);
 	const toggleEdit = () => setIsEditing(!isEditing);
 	return (
 		<div className='model-row'>
-			<div>
-				<label>{`${label}:`}</label>
-				<span>{modelType}</span>
+			<div className='row-header'>
+				<div>
+					<label>{`${label}:`}</label>
+					<span>{modelType}</span>
+				</div>
 				<div className='edit' onClick={toggleEdit}>edit</div>
 			</div>
-			{isEditing ?
-				<Edit value={value} modelType={modelType} setValue={setValue} podType={podType} />
-				: <View value={value} modelType={modelType} setValue={setValue} podType={podType} />
-			}
-			<Meter modelType={modelType} podType={podType} value={value} setValue={setValue} max={max} />
+			<div className='content'>
+				<View value={value} modelType={modelType} setValue={setValue} podType={viewAs} />
+
+				{isEditing &&
+				<div className='edit-panel'>
+					<ViewAsInput value={viewAs} setValue={setViewAs} />
+					<Edit value={value} modelType={modelType} setValue={setValue} podType={viewAs} />
+				</div>
+				}
+				<Meter modelType={modelType} podType={viewAs} value={value} setValue={setValue} max={max} />
+			</div>
 		</div>
 	);
 }
