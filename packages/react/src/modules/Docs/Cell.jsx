f211a2d9c0fca75aef3112a33b30e8b9689e6412
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-09-28 22:34:34 -0400

    styles

--- packages/react/src/modules/Docs/Cell.jsx
+++ packages/react/src/modules/Docs/Cell.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import Fretboard from "../viewers/Fretboard/Fretboard";
 import "./Docs.css";
+import pw_core from '@pw/core';
 
 const Cell = () => {
+	const [a, setA] = useState([0, 0]);
+	const [B, setB] = useState([]);
+	const C = pw_core.models.struct.cell.evaluate({ a, B });
+	const cell = { a, B, C };
 	return (
 		<div className="cell">
-			Cell
+			<Fretboard />
 		</div>
 	);
 };
