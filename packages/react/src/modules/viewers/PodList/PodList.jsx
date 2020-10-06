eb84045212263dc7ab3883b0dc856d8437f98a53
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-06 08:23:27 -0700

    working insert

--- packages/react/src/modules/viewers/PodList/PodList.jsx
+++ packages/react/src/modules/viewers/PodList/PodList.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VectorInput from '../../models/math/vector/VectorInput';
 import VectorOutput from '../../models/math/vector/VectorOutput';
 import IntervalInput from '../../models/theory/interval/IntervalInput';
 import IntervalOutput from '../../models/theory/interval/IntervalOutput';
 import NoteInput from '../../models/theory/note/NoteInput';
 import NoteOutput from '../../models/theory/note/NoteOutput';
+import ButtonInput from '../../models/ui/ButtonInput/ButtonInput';
 import './PodList.css';
 
 const getOutputComponent = (podType) => {
@@ -29,13 +30,37 @@ const getInputComponent = (podType) => {
 	}
 }
 
+const NewPod = ({ value, setValue, podType, i }) => {
+	const [isAdding, setIsAdding] = useState(false);
+	const Component = getInputComponent(podType);
+	const close = () => setIsAdding(false);
+	const insert = r => {
+		close();
+		setValue([...value.slice(0, i), r, ...value.slice(i)]);
+	};
+	return (
+		<div className={`new-pod ${isAdding ? 'active' : ''}`}>
+			{!isAdding && (
+				<ButtonInput onClick={() => setIsAdding(true)} >+</ButtonInput>
+			)}
+			{isAdding && (
+				<Component value={value} setValue={insert} />
+			)}
+		</div>
+	);
+};
+
 const Multi = ({ value, setValue, isEditing, podType }) => {
 	const Component = isEditing ? getInputComponent(podType) : getOutputComponent(podType);
 	return (
 		<div className='multi-preview'>
+			{isEditing && <NewPod value={value} setValue={setValue} podType={podType} i={0} />}
 			{value.map((v, i) => {
 				const setSubValue = r => setValue([...value.slice(0, i), r, ...value.slice(i + 1)]);
-				return <Component value={v} setValue={setSubValue} key={i} />;
+				return <>
+					<Component value={v} setValue={setSubValue} key={i} />
+					{isEditing && <NewPod value={value} setValue={setValue} podType={podType} i={i + 1} />}
+				</>;
 			})}
 		</div>
 	);
