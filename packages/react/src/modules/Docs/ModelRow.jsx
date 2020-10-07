95877cceba3fd2362f41be6585156ba33efa3851
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-06 20:43:16 -0700

    decent styles

--- packages/react/src/modules/Docs/ModelRow.jsx
+++ packages/react/src/modules/Docs/ModelRow.jsx
@@ -37,36 +37,39 @@ const ModelRow = ({ label, modelType, podType, value, setValue }) => {
 					<ButtonInput onClick={() => setOpen(!open)} className='action-button edit'>{open ? '-' : '+'}</ButtonInput>
 				</td>
 			</tr>
-			{open && (
-				<tr>
+			{open && (<>
+				<tr className='submenu'>
 					<td colSpan="4">
-						<div className='subtitle-row'>
+						<div className="space-between">
 							<ViewAsInput value={viewAs} modelType={modelType} setValue={setViewAs} podType={viewAs} />
 							<div className="space" />
-							<ButtonInput active={panelId === 'preview'} className='action-button edit' onClick={() => setPanelId('preview')}>preview</ButtonInput>
-							<ButtonInput active={panelId === 'analyze'} className='action-button edit' onClick={() => setPanelId('analyze')}>analyze</ButtonInput>
-							<ButtonInput active={panelId === 'edit'} className='action-button edit' onClick={() => setPanelId('edit')}>edit</ButtonInput>
-						</div>
-						{panelId === 'preview' &&
-							<div className='panel'>
-								<PodList value={value} modelType={modelType} setValue={setValue} podType={viewAs} isEditing={false} />
+							<div className="action-row">
+								<ButtonInput active={panelId === 'preview'} className='action-button edit' onClick={() => setPanelId('preview')}>preview</ButtonInput>
+								<ButtonInput active={panelId === 'analyze'} className='action-button edit' onClick={() => setPanelId('analyze')}>analyze</ButtonInput>
+								<ButtonInput active={panelId === 'edit'} className='action-button edit' onClick={() => setPanelId('edit')}>edit</ButtonInput>
 							</div>
+						</div>
+					</td>
+				</tr>
+				<tr className='panel'>
+					<td colSpan="4">
+
+						{panelId === 'preview' &&
+							<PodList value={value} modelType={modelType} setValue={setValue} podType={viewAs} isEditing={false} />
 						}
 						{panelId === 'analyze' &&
-							<div className='panel'>
-								<Meter modelType={modelType} podType={viewAs} value={value} setValue={setValue} />
-							</div>
+							<Meter modelType={modelType} podType={viewAs} value={value} setValue={setValue} />
 						}
 						{panelId === 'edit' &&
-							<div className='panel'>
+							<>
 								<ButtonInput className='action-button' onClick={() => setIsLoadingPreset(!isLoadingPreset)}>load preset</ButtonInput>
 								{isLoadingPreset && <MatrixPresetInput value={value} setValue={setValue} modelType={modelType} />}
 								<PodList value={value} modelType={modelType} setValue={setValue} podType={viewAs} isEditing={true} />
-							</div>
+							</>
 						}
 					</td>
 				</tr >
-			)}
+			</>)}
 		</>
 	);
 }
