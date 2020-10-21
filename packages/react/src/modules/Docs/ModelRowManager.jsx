4022956aeb90e03a202b797ee5fd6b675a659499
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-20 21:05:16 -0700

    good

---
+++ packages/react/src/modules/Docs/ModelRowManager.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import ButtonInput from '../ui/ButtonInput/ButtonInput';
+import pw_core from "@pw/core";
+import './ModelTable.css';
+
+const getName = (value, podType) => {
+	if (podType === 'pod') {
+		return value;
+	}
+	else if (podType === 'note') {
+		return pw_core.models.pod.note.getName({ a: value });
+	}
+	else if (podType === 'interval') {
+		return pw_core.models.pod.interval.getName({ a: value });
+	}
+	return null;
+}
+
+const ModelRow = ({ value, setValue, i, isEditing, modelType, podType, theoryType }) => {
+
+	const reduced = pw_core.models.pod.reduce({ a: value });
+	let name = getName(reduced, podType);
+
+	return (
+		<tr key={i}>
+			<td>
+				{i}
+			</td>
+			<td>
+				{JSON.stringify(value)}
+			</td>
+			<td>
+				{JSON.stringify(reduced)}
+			</td>
+			<td>
+				{name}
+			</td>
+			<td>
+				1:1
+			</td>
+		</tr>
+	);
+};
+
+const ModelRowManager = ({ value, setValue, isEditing, modelType, podType, theoryType }) => {
+	if (modelType === 'index') {
+		return <tr><td colSpan="100">{JSON.stringify(value)}</td></tr>
+	}
+	else if (modelType === 'pod') {
+		return <ModelRow
+			value={value}
+			i={0}
+			setValue={setValue}
+			isEditing={isEditing}
+			modelType={modelType}
+			podType={podType}
+			theoryType={theoryType}
+		/>;
+	}
+	else if (modelType === 'podList') {
+		return value.map((v, i) => {
+			const setSubValue = r => setValue([...value.slice(0, i), r, ...value.slice(i + 1)]);
+			//const remove = () => setValue([...value.slice(0, i), ...value.slice(i + 1)]);
+			return <ModelRow
+				key={i}
+				i={i}
+				value={v}
+				setValue={setSubValue}
+				isEditing={isEditing}
+				modelType={modelType}
+				podType={podType}
+				theoryType={theoryType}
+			/>;
+		});
+	}
+};
+
+export default ModelRowManager;
--- packages/react/src/modules/Docs/ModelTable.jsx
+++ packages/react/src/modules/Docs/ModelTable.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import ButtonInput from '../ui/ButtonInput/ButtonInput';
-import ModelIO from './ModelIO';
+import ModelRowManager from './ModelRowManager';
 import pw_core from '@pw/core';
 
 import './ModelTable.css';
@@ -25,16 +25,9 @@ const NewPod = ({ value, setValue, podType, i }) => {
 	);
 };
 
-const reduceModel = (value, modelType) => {
-	if(modelType === 'pod') return pw_core.models.pod.reduce({ a: value });
-	if(modelType === 'podList') return pw_core.models.podList.reduce({ A: value });
-}
-
 const ModelTable = ({ value, setValue, isEditing, modelType, podType, theoryType }) => {
 	const valueText = podType === 'note' ? 'Note' : podType === 'interval' ? 'Interval' : 'Value';
 	const specialText = podType === 'note' ? 'Frequency' : podType === 'interval' ? 'Ratio' : null;
-
-	const reduced = reduceModel(value, modelType);
 
 	return (
 		<table className='model-table'>
@@ -48,48 +41,14 @@ const ModelTable = ({ value, setValue, isEditing, modelType, podType, theoryType
 				</tr>
 			</thead>
 			<tbody>
-				{isEditing && <tr>
-					<td colSpan="5" className="add">
-						<NewPod value={value} setValue={setValue} podType={podType} i={0} />
-					</td>
-				</tr>}
-				{value.map((v, i) => {
-					const setSubValue = r => setValue([...value.slice(0, i), r, ...value.slice(i + 1)]);
-					const remove = () => setValue([...value.slice(0, i), ...value.slice(i + 1)]);
-					return <>
-						<tr>
-							<td>
-								{i}
-							</td>
-							<td>
-								{JSON.stringify(v)}
-							</td>
-							<td>
-								{JSON.stringify(reduced[i])}
-							</td>
-							<td>
-								<ModelIO
-									value={value}
-									setValue={setSubValue}
-									i={i}
-									isEditing={isEditing}
-									modelType={modelType}
-									podType={podType}
-									theoryType={theoryType}
-								/>
-							</td>
-							<td>
-								1:1
-							</td>
-							{isEditing && <td className='pin-right'><ButtonInput onClick={remove} >X</ButtonInput></td>}
-						</tr>
-						{isEditing && <tr>
-							<td colSpan="5" className="add">
-								<NewPod value={value} setValue={setValue} podType={podType} i={i + 1} />
-							</td>
-						</tr>}
-					</>;
-				})}
+				<ModelRowManager
+					value={value}
+					setValue={setValue}
+					isEditing={isEditing}
+					modelType={modelType}
+					podType={podType}
+					theoryType={theoryType}
+				/>
 			</tbody>
 		</table>
 	);
