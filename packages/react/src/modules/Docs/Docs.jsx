a20785db32cc1e5f3c23f3386d9cf0160fbeca60
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-22 20:15:28 -0700

    rework explore

--- packages/react/src/modules/Docs/Docs.jsx
+++ packages/react/src/modules/Docs/Docs.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
-import Explore from "../other/Explore";
+import DropdownInput from '../ui/DropdownInput/DropdownInput';
 import DOCS_CONFIG from "./Docs.config";
 import "./Docs.css";
 import FunctionPanel from "./FunctionPanel";
 
-const FN = DOCS_CONFIG.modules[0].submodules[0].functions[0];
+const MODULE = DOCS_CONFIG.modules.find(x => x.id === 'model');
+const SUBMODULE = MODULE.submodules.find(x => x.id === 'model/pod');
+const FN = SUBMODULE.functions.find(x => x.id === 'model/pod/addPodList');
 
 const Docs = () => {
+	const [module, setModule] = useState(MODULE);
+	const [submodule, setSubmodule] = useState(SUBMODULE);
 	const [fnDef, setFnDef] = useState(FN);
 
 	return (
 		<>
-			<Explore set={setFnDef} />
+			<div>
+				<DropdownInput options={DOCS_CONFIG.modules} value={module} setValue={setModule} />
+				<DropdownInput options={module.submodules} value={submodule} setValue={setSubmodule} />
+				<DropdownInput options={submodule.functions} value={fnDef} setValue={setFnDef} />
+			</div>
 			<FunctionPanel fnDef={fnDef} />
 		</>
 	);
