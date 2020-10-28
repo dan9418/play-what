553f051e4b1a72faf973cd658286c0bc3a29d15a
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-27 20:06:22 -0700

    1

---
+++ packages/react/src/modules/Docs/EditDash.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Docs.css";
+
+const EditDash = ({ isEditing, setIsEditing, actions }) => {
+	return (
+		<div className='submenu'>
+			<div className="space" />
+			<div className="action-row">
+				{isEditing && actions && actions.length && actions.map(a => (
+					<>
+						<div className='edit' onClick={a.onClick}>{a.text}</div>
+						{` | `}
+					</>
+				))}
+				<div className='edit' onClick={() => setIsEditing(!isEditing)}>{isEditing ? 'done' : 'edit'} </div>
+			</div>
+		</div>
+	);
+};
+
+export default EditDash;
--- packages/react/src/modules/Docs/ModelPanel.jsx
+++ packages/react/src/modules/Docs/ModelPanel.jsx
@@ -7,23 +7,7 @@ import pw_core from "@pw/core";
 import DropdownInput from "../ui/DropdownInput/DropdownInput";
 import Fretboard from '../viewers/Fretboard/_module';
 import usePodContext from "../other/PodContext";
-
-const EditDash = ({ isEditing, setIsEditing, actions }) => {
-	return (
-		<div className='submenu'>
-			<div className="space" />
-			<div className="action-row">
-				{isEditing && actions && actions.length && actions.map(a => (
-					<>
-						<div className='edit' onClick={a.onClick}>{a.text}</div>
-						{` | `}
-					</>
-				))}
-				<div className='edit' onClick={() => setIsEditing(!isEditing)}>{isEditing ? 'done' : 'edit'} </div>
-			</div>
-		</div>
-	);
-};
+import EditDash from "./EditDash";
 
 const getPreviewText = (value, modelType, podType, theoryType) => {
 	if (modelType === 'pod') {
