398476432e72d1b551b5f77898452c21fd00eb65
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-10-28 20:44:03 -0700

    convert podList

--- packages/core/src/modules/models/podList/podList.js
+++ packages/core/src/modules/models/podList/podList.js
@@ -9,70 +9,68 @@ import interval from '../pod/interval';
 
 const DEFAULT = [[0, 0]];
 
-// Common
+class PodList {
 
-const isValid = (podList) => {
-	return podList !== null && Array.isArray(podList) && podList.find((v) => !pod.isValid(v));
-};
-
-const areEqual = ({ list1, list2 }) => {
-	if (!list1 || !list2) return false;
-	if (list1.length !== list2.length) return false;
-	for (let i = 0; i < list1.length; i++) {
-		if (!pod.areEqual({ pod1: list1[i], pod2: list2[i] })) { return false; }
+	constructor(value) {
+		this.value = value;
 	}
-	return true;
-};
 
-const reduce = ({ A }) => A.map((a) => pod.reduce({ a }));
+	// Common
 
-const getName = ({ podList, podType, theoryType }) => {
-	if (theoryType) {
-		switch (theoryType) {
-		case 'chord':
-			return chord.getName({ A: podList });
-		case 'scale':
-			return scale.getName({ A: podList });
+	static isValid(podList) {
+		return podList !== null && Array.isArray(podList) && podList.find((v) => !pod.isValid(v));
+	};
+
+	static areEqual({ list1, list2 }) {
+		if (!list1 || !list2) return false;
+		if (list1.length !== list2.length) return false;
+		for (let i = 0; i < list1.length; i++) {
+			if (!pod.areEqual({ pod1: list1[i], pod2: list2[i] })) { return false; }
 		}
-	}
-	if (podType) {
-		switch (podType) {
-		case 'note':
-			return podList.map(pod => note.getName({ a: pod })).join(', ');
-		case 'interval':
-			return podList.map(pod => interval.getName({ a: pod })).join(', ');
+		return true;
+	};
+
+	static reduce({ A }) { return A.map((a) => pod.reduce({ a })); }
+
+	static getName({ podList, podType, theoryType }) {
+		if (theoryType) {
+			switch (theoryType) {
+			case 'chord':
+				return chord.getName({ A: podList });
+			case 'scale':
+				return scale.getName({ A: podList });
+			}
 		}
+		if (podType) {
+			switch (podType) {
+			case 'note':
+				return podList.map(pod => note.getName({ a: pod })).join(', ');
+			case 'interval':
+				return podList.map(pod => interval.getName({ a: pod })).join(', ');
+			}
+		}
+		return JSON.stringify(podList);
 	}
-	return JSON.stringify(podList);
+
+	// Utils
+
+	static findPodWithPitch({ A, p }) {
+		const octaveReduce = true
+		const pitch = octaveReduce ? index.modulo({ a: p, b: pod.max[0] }) : p;
+		return A.find((n) => n[0] === pitch) || null;
+	};
+
+	static findIndexOfPodWithPitch({ A, p }) {
+		const octaveReduce = true
+		const pitch = octaveReduce ? index.modulo({ a: p, b: pod.max[0] }) : p;
+		return A.findIndex((n) => n[0] === pitch);
+	};
+
+	static addPod({ A, b }) { return A.map((a) => pod.addPod({ a, b })); }
+
 }
 
-// Utils
+PodList.chord = chord;
+PodList.scale = scale;
 
-const findPodWithPitch = ({ A, p }) => {
-	const octaveReduce = true
-	const pitch = octaveReduce ? index.modulo({ a: p, b: pod.max[0] }) : p;
-	return A.find((n) => n[0] === pitch) || null;
-};
-
-const findIndexOfPodWithPitch = ({ A, p }) => {
-	const octaveReduce = true
-	const pitch = octaveReduce ? index.modulo({ a: p, b: pod.max[0] }) : p;
-	return A.findIndex((n) => n[0] === pitch);
-};
-
-const addPod = ({ A, b }) => A.map((a) => pod.addPod({ a, b }));
-
-export default {
-	chord,
-	scale,
-	defaultValue: DEFAULT,
-	getName,
-	// Common
-	isValid,
-	areEqual,
-	reduce,
-	// Utils
-	findPodWithPitch,
-	findIndexOfPodWithPitch,
-	addPod
-};
+export default PodList;
