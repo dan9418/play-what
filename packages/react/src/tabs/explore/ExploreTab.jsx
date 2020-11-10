973c0d3af05d4a45c252ccfc2de799eb88931d12
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-11-09 21:48:57 -0800

    start breadcrumbs

--- packages/react/src/tabs/explore/ExploreTab.jsx
+++ packages/react/src/tabs/explore/ExploreTab.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ModelPanel from '../explore/ModelPanel';
-import { PodContextProvider } from '../../other/PodContext';
+import usePodContext, { PodContextProvider } from '../../other/PodContext';
 import './ExploreTab.css';
+import ButtonInput from '../../ui/ButtonInput/ButtonInput';
+
+const Breadcrumbs = () => {
+	const podContext = usePodContext();
+	return (
+		<div className='breadcrumb-container'>
+			{podContext.breadcrumbs.map(b => <ButtonInput key={b}>{b}</ButtonInput>)}
+		</div>
+	);
+};
 
 const Explore = () => {
 	return (
 		<div className='explore-tab'>
 			<PodContextProvider>
+				<Breadcrumbs />
 				<ModelPanel />
 			</PodContextProvider>
 		</div>
