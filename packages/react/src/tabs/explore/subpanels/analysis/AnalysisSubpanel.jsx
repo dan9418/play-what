6585c1485a96102b4d6307ab781d256a854ba6fd
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-11-16 20:25:19 -0800

    fix graph

--- packages/react/src/tabs/explore/subpanels/analysis/AnalysisSubpanel.jsx
+++ packages/react/src/tabs/explore/subpanels/analysis/AnalysisSubpanel.jsx
@@ -3,6 +3,7 @@ import useEditContext from "../../../../other/EditContext";
 import usePodContext from "../../../../other/PodContext";
 import DropdownInput from "../../../../ui/DropdownInput/DropdownInput";
 import Fretboard from "../../../../viewers/Fretboard/Fretboard";
+import Graph from "../../../../viewers/Graph/Graph";
 import Keyboard from "../../../../viewers/Keyboard/Keyboard";
 import Meter from "../../../../viewers/Meter/Meter";
 import Subpanel from "../../Subpanel";
@@ -22,12 +23,17 @@ const VIEWERS = [
 		id: 'keyboard',
 		name: 'Keyboard',
 		component: Keyboard
+	},
+	{
+		id: 'graph',
+		name: 'Graph',
+		component: Graph
 	}
 ];
 
 const ViewerBox = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [viewer, setViewer] = useState(VIEWERS[2]);
+	const [viewer, setViewer] = useState(VIEWERS[3]);
 	const podContext = usePodContext();
 	const { value, setValue, podType, isList } = podContext;
 	const { isEditing } = useEditContext();
---
+++ packages/react/src/viewers/Graph/Graph.jsx
@@ -0,0 +1,71 @@
+import PodUtils from '@pw/core/src/modules/PodUtils';
+import { SCALE } from '@pw/core/src/modules/Presets';
+import React from 'react';
+import { MAX } from '../../../../core/src/modules/Constants';
+import './Graph.css';
+
+const Cell = props => {
+	const { color } = props;
+	return (
+		<div className='cell'>
+			{color && <div className='point' style={{ backgroundColor: color }} />}
+		</div>
+	);
+};
+
+const Label = ({ axis, children }) => {
+	return (
+		<div className={`label ${axis}`}>
+			{children}
+		</div>
+	);
+};
+
+const getCells = (origin, podContext) => {
+	const { value, podType, isList } = podContext;
+	const cells = [];
+	for (let p = MAX[0] - 1; p >= -1; p--) {
+		cells.push(<Label key={'p' + p} axis='y'>{p > -1 && p}</Label>)
+		for (let d = 0; d < MAX[1]; d++) {
+			if (p === -1) {
+				cells.push(<Label key={'d' + d} axis='x'>{d}</Label>)
+			}
+			else {
+				const point = [p, d];
+				const isOrigin = PodUtils.areEqual(origin, point);
+				const isResultant = value.findIndex(v => PodUtils.areEqual(v, point)) >= 0;
+				cells.push(<Cell key={d + '-' + p} color={isOrigin ? 'red' : isResultant ? 'blue' : null} />)
+			}
+		}
+	}
+	return cells;
+};
+
+const DEFAULT_PROPS = {
+	origin: [0, 0],
+	title: 'd vs. p',
+	xLabel: 'd',
+	yLabel: 'p',
+	podContext: {
+		value: SCALE.Major.value,
+		podType: 'note',
+		isList: true
+	}
+}
+
+const Graph = userProps => {
+	const props = { ...DEFAULT_PROPS, ...userProps };
+	const { origin, title, xLabel, yLabel, podContext } = props;
+	return (
+		<div className="graph-container">
+			<div className="graph-title">{title}</div>
+			<div style={{ display: 'flex', justifyContent: 'center' }}>
+				<div className="y-label">{yLabel}</div>
+				<div className="graph">{getCells(origin, podContext)}</div>
+			</div>
+			<div className="x-label">{xLabel}</div>
+		</div>
+	);
+}
+
+export default Graph;
