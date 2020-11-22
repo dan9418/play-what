8be11e16b21f20eb1c568c6ad35ca99d7ff4a4d9
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-11-21 20:25:06 -0800

    wip

--- packages/react/src/tabs/explore/panels/OutputPanel.jsx
+++ packages/react/src/tabs/explore/panels/OutputPanel.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import useEditContext from "../../../other/EditContext";
 import { OutputContextProvider } from "../../../other/OutputContext";
+import ButtonInput from "../../../ui/ButtonInput/ButtonInput";
 import ViewerSubpanel from "../subpanels/viewers/ViewerSubpanel";
 import Panel from "./Panel";
+
+
+const SubpanelWrapper = ({ children, isLast }) => {
+	const { isEditing } = useEditContext();
+	return (
+		<div className="subpanel-wrapper">
+			{isEditing && <ButtonInput>+ Add</ButtonInput>}
+			<div className="edit-wrapper">
+
+				{children}
+
+				{isEditing && <ButtonInput>X</ButtonInput>}
+			</div>
+			{isLast && isEditing && <ButtonInput>+ Add</ButtonInput>}
+		</div>
+	);
+}
+
 
 const OutputPanel = ({ inputs, outputs, setOutputs }) => {
 	return (
@@ -9,9 +29,11 @@ const OutputPanel = ({ inputs, outputs, setOutputs }) => {
 			{outputs.map((output, i) => {
 				const setOutput = output => setOutputs([...outputs.slice(0, i), output, ...outputs.slice(i + 1)]);
 				return (
-					<OutputContextProvider key={i} output={output} setOutput={setOutput} inputs={inputs}>
-						<ViewerSubpanel name={`Out ${i + 1}`} />
-					</OutputContextProvider>
+					<SubpanelWrapper key={i} isLast={i === output.length - 1}>
+						<OutputContextProvider output={output} setOutput={setOutput} inputs={inputs}>
+							<ViewerSubpanel name={`Out ${i + 1}`} />
+						</OutputContextProvider>
+					</SubpanelWrapper>
 				);
 			})}
 		</Panel>
