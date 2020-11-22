a30f6727688b521f63991992c6ae5e3cb39e97e1
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-11-22 13:13:51 -0800

    add docs page

--- packages/react/src/tabs/docs/DocsTab.jsx
+++ packages/react/src/tabs/docs/DocsTab.jsx
@@ -1,2 +1,51 @@
-const Docs = () => null;
+import React from 'react';
+import './Docs.css';
+
+const Docs = () => {
+	return (
+		<div className='docs'>
+			<h1>API Documentation</h1>
+			<h2>Coming Soon!</h2>
+			<p>
+                For now, here are some useful links:
+			</p>
+			<ul>
+				<li>
+					<a href="https://github.com/dan9418/play-what">Current Monorepo</a>
+				</li>
+				<li>Beta</li>
+				<ul>
+					<li>Sample</li>
+					<ul>
+						<li>
+							<a href="https://github.com/dan9418/play-what-sample">Demo</a>
+						</li>
+						<li>
+							<a href="https://github.com/dan9418/play-what-sample">Repo</a>
+						</li>
+					</ul>
+					<li>Sandbox</li>
+					<ul>
+						<li>
+							<a href="https://github.com/dan9418/play-what-sandbox">Demo</a>
+						</li>
+						<li>
+							<a href="https://dan9418.github.io/play-what-sandbox/">Repo</a>
+						</li>
+					</ul>
+					<li>Packages</li>
+					<ul>
+						<li>
+							<a href="https://github.com/dan9418/play-what-beta">Repo (Core)</a>
+						</li>
+						<li>
+							<a href="https://github.com/dan9418/play-what-react-viewers">Repo (React)</a>
+						</li>
+					</ul>
+				</ul>
+			</ul>
+		</div>
+	)
+};
+
 export default Docs;
