6712838956ebe6d099cf44b9891bb917469f454d
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-12-08 08:16:25 -0800

    refactor

--- packages/react/src/tabs/explore/Timeline/Timeline.jsx
+++ packages/react/src/tabs/explore/Timeline/Timeline.jsx
@@ -4,15 +4,26 @@ import 'react-hot-loader';
 import './Timeline.css';
 
 const Timeline = ({ frames, frameIndex, setFrameIndex }) => {
+	const measureScope = 4;
+
+	const beatNum = 4;
+	const beatType = 4;
 	return (
 		<>
 			<section className="timeline">
 				<section className="playback-bar">
+					<span>{`${beatNum}/${beatType}`}</span>
 					<button>Play</button>
 					<button>Pause</button>
 					<button>Stop</button>
 					<button>Prev</button>
 					<button>Next</button>
+				</section>
+				<section className="m-bar">
+					{(new Array(measureScope)).map((m, i) => {
+						const classes = ['m'];
+						return <div key={i} className={classes.join(' ')} />;
+					})}
 				</section>
 				<section className="frame-bar">
 					{frames.map((frame, i) => {
