386a4d2b821ff2472675931ba36a1941985a6bf0
Dan Bednarczyk <dpbednarczyk@gmail.com>
2020-12-10 21:57:50 -0800

    frames

--- packages/react/src/tabs/explore/Timeline/Timeline.jsx
+++ packages/react/src/tabs/explore/Timeline/Timeline.jsx
@@ -1,38 +1,9 @@
 
 import { COLOR_FN } from '@pw/core/src/Color.constants';
-import ColorUtils from '@pw/core/src/Color.utils';
 import React from 'react';
-import ButtonInput from '../../../ui/ButtonInput/ButtonInput';
 import styled from 'styled-components';
+import ButtonInput from '../../../ui/ButtonInput/ButtonInput';
 import './Timeline.css';
-
-const getPrevPosition = (position, measures) => {
-	const [mIndex, bIndex] = position;
-	const firstMeasure = mIndex === 0;
-	const firstBeat = bIndex === 0;
-
-	if (firstMeasure && firstBeat) {
-		return [measures.length - 1, measures[mIndex].length - 1];
-	}
-	if (firstBeat) {
-		return [mIndex - 1, measures[mIndex].beats.length - 1];
-	}
-	return [mIndex, bIndex - 1];
-}
-
-const getNextPosition = (position, measures) => {
-	const [mIndex, bIndex] = position;
-	const lastMeasure = mIndex === measures.length - 1;
-	const lastBeat = bIndex === measures[mIndex].beats.length - 1;
-
-	if (lastMeasure && lastBeat) {
-		return [0, 0];
-	}
-	if (lastBeat) {
-		return [mIndex + 1, 0];
-	}
-	return [mIndex, bIndex + 1];
-}
 
 const StyledTable = styled.table`
 	background-color: white;
@@ -92,80 +63,61 @@ const PitchCell = styled.td`
 
 const PITCHES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-const Timeline = ({ frameset, position, setPosition }) => {
-	const [mIndex, bIndex] = position;
+const StyledTimeline = styled.section`
+	width: 100%;
+    background-color: #fff;
+    border-top: 1px solid #555;
+    position: fixed;
+    bottom: 0;
+    z-index: 1000;
+`;
 
-	const { measures } = frameset;
-	const curMeasure = measures[mIndex];
-	const { beats } = curMeasure
-	const curBeat = beats[bIndex];
-
-	const beatNum = 4;
-	const beatType = 4;
+const Timeline = ({ frames, index, setIndex }) => {
 
 	return (
 		<>
-			<section className="timeline">
+			<StyledTimeline>
 				<PlaybackBar>
 					<Left>
-						<span>{`${beatNum}/${beatType}`}</span>
+						{index}
 					</Left>
 					<Right>
-						<ButtonInput onClick={() => setPosition(getPrevPosition(position, frameset.measures))}>Prev</ButtonInput>
-						<ButtonInput onClick={() => setPosition(getNextPosition(position, frameset.measures))}>Next</ButtonInput>
+						<ButtonInput onClick={() => setIndex(index - 1)}>Prev</ButtonInput>
+						<ButtonInput onClick={() => setIndex(index + 1)}>Next</ButtonInput>
 					</Right>
 				</PlaybackBar>
 				<StyledTable>
 					<thead>
 						<tr>
-							{measures.map((measure, i) => {
-								const mActive = i === mIndex;
+							{frames.map((frame, b) => {
+								const isActive = b === index;
 								return (
-									<React.Fragment key={i}>
-										{measure.beats.map((beat, j) => {
-											const bActive = mActive && j === bIndex;
-											return (
-												<BeatTab
-													key={j}
-													$active={bActive}
-													onClick={() => setPosition([i, j])}>
-													{j}
-												</BeatTab>
-											);
-										})}
-									</React.Fragment>
+									<BeatTab
+										key={b}
+										$active={isActive}
+										onClick={() => setIndex(b)}
+									>
+										{b}
+									</BeatTab>
 								);
 							})}
 						</tr>
 					</thead>
 					<tbody>
 						{PITCHES.map((pitch, p) => {
-							const pClasses = ['pitch'];
-							const pActive = false;
 							return (
-								<PitchRow key={p} className={pClasses.join(' ')} >
-									{measures.map((measure, i) => {
-										const mClasses = ['measure'];
-										const mActive = i === mIndex;
-
+								<PitchRow key={p}>
+									{frames.map((frame, b) => {
+										const isActive = b === index;
+										const pod = frame ?
+											frame.pods.find(x => x[0] === p)
+											: null;
 										return (
-											<React.Fragment key={i}>
-												{measure.beats.map((beat, j) => {
-													const bClasses = ['beat'];
-													const bActive = mActive && j === bIndex;
-													const pod = beat ?
-														beat.pods.find(x => x[0] === p)
-														: null;
-													return (
-														<PitchCell
-															key={j}
-															className={bClasses.join(' ')}
-															$active={bActive}
-															$pod={pod}
-														/>
-													);
-												})}
-											</React.Fragment>
+											<PitchCell
+												key={b}
+												$active={isActive}
+												$pod={pod}
+											/>
 										);
 									})}
 								</PitchRow>
@@ -173,7 +125,7 @@ const Timeline = ({ frameset, position, setPosition }) => {
 						})}
 					</tbody>
 				</StyledTable>
-			</section>
+			</StyledTimeline>
 		</>
 	);
 };
