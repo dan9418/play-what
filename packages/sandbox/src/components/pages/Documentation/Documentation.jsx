45bc68ea911edc750c79e90e96d38d1a75bdbc66
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-01-01 20:51:55 -0500

    details

--- packages/sandbox/src/components/pages/Documentation/Documentation.jsx
+++ packages/sandbox/src/components/pages/Documentation/Documentation.jsx
@@ -28,6 +28,12 @@ border-radius: 16px;
 	& ul {
 		margin-left: 16px;
 	}
+	& details {
+		padding: 4px;
+		& summary {
+			cursor: pointer;
+		}
+	}
 `;
 
 const Documentation = () => {
@@ -42,8 +48,6 @@ const Documentation = () => {
 					Details will be published here when available.
 					For now, here are some useful links:
 				</p>
-
-				<h3>Current</h3>
 				<ul>
 					<li>
 						<a href="https://dan9418.github.io/play-what/">App</a>
@@ -52,36 +56,38 @@ const Documentation = () => {
 						<a href="https://github.com/dan9418/play-what">Monorepo</a>
 					</li>
 				</ul>
-				<h3>Beta</h3>
-				<ul>
-					<li>Sample</li>
+				<details>
+					<summary>Beta Version</summary>
 					<ul>
-						<li>
-							<a href="https://github.com/dan9418/play-what-sample">Demo</a>
-						</li>
-						<li>
-							<a href="https://github.com/dan9418/play-what-sample">Repo</a>
-						</li>
+						<li>Sample</li>
+						<ul>
+							<li>
+								<a href="https://github.com/dan9418/play-what-sample">Demo</a>
+							</li>
+							<li>
+								<a href="https://github.com/dan9418/play-what-sample">Repo</a>
+							</li>
+						</ul>
+						<li>Sandbox</li>
+						<ul>
+							<li>
+								<a href="https://github.com/dan9418/play-what-sandbox">Demo</a>
+							</li>
+							<li>
+								<a href="https://dan9418.github.io/play-what-sandbox/">Repo</a>
+							</li>
+						</ul>
+						<li>Packages</li>
+						<ul>
+							<li>
+								<a href="https://github.com/dan9418/play-what-beta">Repo (Core)</a>
+							</li>
+							<li>
+								<a href="https://github.com/dan9418/play-what-react-viewers">Repo (React)</a>
+							</li>
+						</ul>
 					</ul>
-					<li>Sandbox</li>
-					<ul>
-						<li>
-							<a href="https://github.com/dan9418/play-what-sandbox">Demo</a>
-						</li>
-						<li>
-							<a href="https://dan9418.github.io/play-what-sandbox/">Repo</a>
-						</li>
-					</ul>
-					<li>Packages</li>
-					<ul>
-						<li>
-							<a href="https://github.com/dan9418/play-what-beta">Repo (Core)</a>
-						</li>
-						<li>
-							<a href="https://github.com/dan9418/play-what-react-viewers">Repo (React)</a>
-						</li>
-					</ul>
-				</ul>
+				</details>
 
 			</StyledSourceCode>
 		</StyledDocumentation>
