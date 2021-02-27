422e678733947b7200836b7393e2cc992c4db889
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-02-26 19:35:11 -0800

    basic rhythym

--- packages/sandbox/src/components/core/PlaybackControls.jsx
+++ packages/sandbox/src/components/core/PlaybackControls.jsx
@@ -1,5 +1,5 @@
 import { MODEL } from '@pw/core/src/models/helpers/Model.constants';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import styled from 'styled-components';
 import { usePathNavContext } from '../../contexts/PathNavContext';
@@ -23,11 +23,29 @@ const StyledPlaybackControls = styled.div`
 const PlaybackControls = () => {
 	const { modelId, modelData } = useRecoilValue(pathHeadState);
 	const { prev, next } = usePathNavContext();
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [tempo, setTempo] = useState(60);
 
 	const model = MODEL[modelId];
 
+	useEffect(() => {
+		if (!isPlaying || !model.utils.playSound) return;
+		model.utils.playSound(modelData);
+	}, [isPlaying, modelData]);
+
+	useEffect(() => {
+		if (!isPlaying) return;
+		const secondsPerBeat = 60 / tempo;
+
+		console.log(secondsPerBeat);
+
+		setTimeout(next, secondsPerBeat * 1000);
+	}, [isPlaying, modelData]);
+
 	return (
 		<StyledPlaybackControls>
+			<div>Tempo: {tempo}</div>
+			<ButtonInput onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? 'Stop' : 'Start'}</ButtonInput>
 			{prev && <ButtonInput onClick={prev}>Prev</ButtonInput>}
 			{next && <ButtonInput onClick={next}>Next</ButtonInput>}
 			{model.utils.playSound && <ButtonInput onClick={() => model.utils.playSound(modelData)}>Play Sound</ButtonInput>}
