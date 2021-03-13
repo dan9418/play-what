743387b134b955738343a3de8016817186593504
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-03-12 20:14:31 -0800

    basic action box

--- packages/sandbox/src/components/core/ActionBox.jsx
+++ packages/sandbox/src/components/core/ActionBox.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import ButtonInput from '../ui/inputs/buttons/ButtonInput';
 import PresetSelector from './PresetSelector';
 import SupersetSelector from './SupersetSelector';
 
@@ -17,19 +18,49 @@ const StyledActionBox = styled.div`
 	padding: 8px;
 `;
 
+const ACTIONS = [
+	{
+		name: 'Load Preset',
+		component: PresetSelector
+	},
+	{
+		name: 'Apply Superset',
+		component: PresetSelector
+	}
+];
+
 const ActionBox = ({ modelId, modelValue, modelRoot, setModel, setSuperset }) => {
+	const [selectedActionIndex, setSelectedActionIndex] = useState(null);
+
+	let content = null;
+	if (selectedActionIndex === null) {
+		content = (
+			<StyledActionList>
+				{ACTIONS.map((a, i) => {
+					return (
+						<li key={a.name}>
+							<ButtonInput onClick={() => setSelectedActionIndex(i)}>{a.name}</ButtonInput>
+						</li>
+					);
+				})}
+			</StyledActionList>
+		);
+	}
+	else {
+		const action = ACTIONS[selectedActionIndex];
+		const Component = action.component;
+		content = <>
+			<h3>
+				{action.name}
+				<ButtonInput onClick={() => setSelectedActionIndex(null)}>X</ButtonInput>
+			</h3>
+			<Component modelId={modelId} modelValue={modelValue} modelRoot={modelRoot} setModel={setModel} setSuperset={setSuperset} />
+		</>
+	}
 
 	return (
 		<StyledActionBox>
-			<StyledActionList>
-				<li>Load Preset</li>
-				<li>Apply Superset</li>
-				<li>Transpose</li>
-				<li>Inversion</li>
-			</StyledActionList>
-
-			<PresetSelector modelId={modelId} modelValue={modelValue} modelRoot={modelRoot} setModel={setModel} />
-			<SupersetSelector modelId={modelId} modelValue={modelValue} modelRoot={modelRoot} setSuperset={setSuperset} />
+			{content}
 		</StyledActionBox>
 	);
 };
