04e3edacc401248be8cc0ac10281b9c3b10427f9
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-03-15 23:27:12 -0700

    presets

--- packages/sandbox/src/components/core/PresetSelector.jsx
+++ packages/sandbox/src/components/core/PresetSelector.jsx
@@ -7,6 +7,7 @@ import ButtonInput from '../ui/inputs/buttons/ButtonInput';
 import DropdownInput from '../ui/inputs/DropdownInput';
 
 const PresetSelector = ({ modelId, modelValue, modelRoot, setModel }) => {
+	const [modelIndex, setModelIndex] = useState(0);
 	const [index, setIndex] = useState(0);
 	const { parent } = useRecoilValue(pathState);
 
@@ -15,11 +16,9 @@ const PresetSelector = ({ modelId, modelValue, modelRoot, setModel }) => {
 		parentModel = MODEL[parent.modelId];
 	}
 	const modelOptions = parentModel.validChildren.map(x => ({ value: x, name: MODEL[x].name }));
+	const selectedModelOption = modelOptions[modelIndex];
 
-	const model = MODEL[modelId];
-	const options = model.presets;
-
-	if (!options || !options.length) return 'No presets';
+	const options = MODEL[selectedModelOption.value].presets;
 
 	const value = options[index];
 	const setValue = () => setModel(value.value);
@@ -27,7 +26,7 @@ const PresetSelector = ({ modelId, modelValue, modelRoot, setModel }) => {
 
 	return (
 		<>
-			<DropdownInput options={modelOptions} value={value} setValue={setSelection} />
+			<DropdownInput options={modelOptions} value={selectedModelOption} setValue={(v, i) => setModelIndex(i)} />
 			<DropdownInput options={options} value={value} setValue={setSelection} />
 			<ButtonInput onClick={setValue}>
 				<Icon iconId='confirm' color='white' />
