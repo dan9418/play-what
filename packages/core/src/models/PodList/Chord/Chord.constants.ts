813bf6842395737be91908f0c860ead22d265d6e
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-04-11 11:53:23 -0700

    chord id

--- packages/core/src/models/PodList/Chord/Chord.constants.ts
+++ packages/core/src/models/PodList/Chord/Chord.constants.ts
@@ -1,24 +1,52 @@
+import { IntervalId } from './../../Pod/Interval/Interval.constants';
 import { IPod, IModelPreset } from './../../Model.constants';
 import { INTERVAL } from "../../Pod/Interval/Interval.constants";
 
+export enum ChordId {
+	MajTri = 'MajTri',
+	Maj6 = 'Maj6',
+	Maj7 = 'Maj7',
+	MinTri = 'MinTri',
+	Min6 = 'Min6',
+	Min7 = 'Min7',
+	MinMaj7 = 'MinMaj7',
+	Dom7 = 'Dom7',
+	Dom7b9 = 'Dom7b9',
+	AugTri = 'AugTri',
+	Aug7 = 'Aug7',
+	AugM7 = 'AugM7',
+	DimTri = 'DimTri',
+	Dim7 = 'Dim7',
+	HalfDim7 = 'HalfDim7',
+	Sus2 = 'Sus2',
+	Sus4 = 'Sus4',
+}
+
+const formatPreset = (id: ChordId, name: string, intervalIds: IntervalId[]) => ({
+	id,
+	name,
+	value: intervalIds.map(id => INTERVAL[id].value)
+});
+
+
 export const CHORD: { [x: string]: IModelPreset<IPod[]> } = {
-	Maj: { id: 'Maj', name: 'Major Triad', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.P5.value] },
-	Maj6: { id: 'Maj6', name: 'Major 6th', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.P5.value, INTERVAL.M6.value] },
-	Maj7: { id: 'Maj7', name: 'Major 7th', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.P5.value, INTERVAL.M7.value] },
-	Min: { id: 'Min', name: 'Minor Triad', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.P5.value] },
-	Min6: { id: 'Min6', name: 'Minor 6th', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.P5.value, INTERVAL.M6.value] },
-	Min7: { id: 'Min7', name: 'Minor 7th', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.P5.value, INTERVAL.m7.value] },
-	MinMaj7: { id: 'MinMaj7', name: 'Minor-Major 7th', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.P5.value, INTERVAL.M7.value] },
-	Dom7: { id: 'Dom7', name: 'Dominant 7th', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.P5.value, INTERVAL.m7.value] },
-	Dom7b9: { id: 'Dom7b9', name: 'Dominant 7th (b9)', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.P5.value, INTERVAL.m7.value, INTERVAL.m2.value] },
-	Aug: { id: 'Aug', name: 'Augmented Triad', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.A5.value] },
-	Aug7: { id: 'Aug7', name: 'Augmented 7th', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.A5.value, INTERVAL.m7.value] },
-	AugM7: { id: 'AugM7', name: 'Augmented Major 7th', value: [INTERVAL.P1.value, INTERVAL.M3.value, INTERVAL.A5.value, INTERVAL.M7.value] },
-	Dim: { id: 'Dim', name: 'Diminished Triad', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.d5.value] },
-	Dim7: { id: 'Dim7', name: 'Diminished 7th', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.d5.value, INTERVAL.d7.value] },
-	HalfDim7: { id: 'HalfDim7', name: 'Half-Diminished 7th', value: [INTERVAL.P1.value, INTERVAL.m3.value, INTERVAL.d5.value, INTERVAL.m7.value] },
-	Sus2: { id: 'Sus2', name: 'Suspended 2nd', value: [INTERVAL.P1.value, INTERVAL.M2.value, INTERVAL.P5.value] },
-	Sus4: { id: 'Sus4', name: 'Suspended 4th', value: [INTERVAL.P1.value, INTERVAL.P4.value, INTERVAL.P5.value] }
+	[ChordId.MajTri]: formatPreset(ChordId.MajTri, 'Major Triad', [IntervalId.P1, IntervalId.M3, IntervalId.P5]),
+	[ChordId.Maj6]: formatPreset(ChordId.Maj6, 'Major 6th', [IntervalId.P1, IntervalId.M3, IntervalId.P5, IntervalId.M6]),
+	[ChordId.Maj7]: formatPreset(ChordId.Maj7, 'Major 7th', [IntervalId.P1, IntervalId.M3, IntervalId.P5, IntervalId.M7]),
+	[ChordId.MinTri]: formatPreset(ChordId.MinTri, 'Minor Triad', [IntervalId.P1, IntervalId.m3, IntervalId.P5]),
+	[ChordId.Min6]: formatPreset(ChordId.Min6, 'Minor 6th', [IntervalId.P1, IntervalId.m3, IntervalId.P5, IntervalId.M6]),
+	[ChordId.Min7]: formatPreset(ChordId.Min7, 'Minor 7th', [IntervalId.P1, IntervalId.m3, IntervalId.P5, IntervalId.m7]),
+	[ChordId.MinMaj7]: formatPreset(ChordId.MinMaj7, 'Minor-Major 7th', [IntervalId.P1, IntervalId.m3, IntervalId.P5, IntervalId.M7]),
+	[ChordId.Dom7]: formatPreset(ChordId.Dom7, 'Dominant 7th', [IntervalId.P1, IntervalId.M3, IntervalId.P5, IntervalId.m7]),
+	[ChordId.Dom7b9]: formatPreset(ChordId.Dom7b9, 'Dominant 7th (b9)', [IntervalId.P1, IntervalId.M3, IntervalId.P5, IntervalId.m7, IntervalId.m2]),
+	[ChordId.AugTri]: formatPreset(ChordId.AugTri, 'Augmented Triad', [IntervalId.P1, IntervalId.M3, IntervalId.A5]),
+	[ChordId.Aug7]: formatPreset(ChordId.Aug7, 'Augmented 7th', [IntervalId.P1, IntervalId.M3, IntervalId.A5, IntervalId.m7]),
+	[ChordId.AugM7]: formatPreset(ChordId.AugM7, 'Augmented Major 7th', [IntervalId.P1, IntervalId.M3, IntervalId.A5, IntervalId.M7]),
+	[ChordId.DimTri]: formatPreset(ChordId.DimTri, 'Diminished Triad', [IntervalId.P1, IntervalId.m3, IntervalId.d5]),
+	[ChordId.Dim7]: formatPreset(ChordId.Dim7, 'Diminished 7th', [IntervalId.P1, IntervalId.m3, IntervalId.d5, IntervalId.d7]),
+	[ChordId.HalfDim7]: formatPreset(ChordId.HalfDim7, 'Half-Diminished 7th', [IntervalId.P1, IntervalId.m3, IntervalId.d5, IntervalId.m7]),
+	[ChordId.Sus2]: formatPreset(ChordId.Sus2, 'Suspended 2nd', [IntervalId.P1, IntervalId.M2, IntervalId.P5]),
+	[ChordId.Sus4]: formatPreset(ChordId.Sus4, 'Suspended 4th', [IntervalId.P1, IntervalId.P4, IntervalId.P5])
 };
 
 export const CHORD_VALUES = Object.values(CHORD);
