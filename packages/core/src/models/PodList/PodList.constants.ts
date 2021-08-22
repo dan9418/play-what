a11fd046a91ba2ee27370dffbe070c93d19ce1ae
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-08-22 08:22:15 -0700

    master presets

--- packages/core/src/models/PodList/PodList.constants.ts
+++ packages/core/src/models/PodList/PodList.constants.ts
@@ -1,1 +1,15 @@
-const MASTER_PRESETS = [];
+import { IPod, IPreset } from "../Model.constants";
+import { INTERVAL_PRESETS } from "../Pod/Interval/Interval.constants";
+import { CHORD_PRESETS } from "./Chord/Chord.constants";
+import { SCALE_PRESETS } from "./Scale/Scale.constants";
+
+const podToPodList = (podPresets: IPreset<IPod>[]): IPreset<IPod[]>[] =>
+    podPresets.map(preset => ({ ...preset, value: [preset.value] }))
+
+const MASTER_PRESETS: IPreset<IPod[]>[] = [
+    ...podToPodList(INTERVAL_PRESETS),
+    ...CHORD_PRESETS,
+    ...SCALE_PRESETS
+];
+
+export default MASTER_PRESETS;
