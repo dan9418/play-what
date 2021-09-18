4091ae24667d8dc05f86b63c723715fa49a769b8
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-09-17 17:47:09 -0700

    ui

--- packages/app/src/components/explore/nav/QuickLink.tsx
+++ packages/app/src/components/explore/nav/QuickLink.tsx
@@ -1,30 +1,40 @@
-import IconButton from "@pw/ui/src/inputs/IconButton";
+import ButtonInput from "@pw/ui/src/inputs/ButtonInput";
 import React from "react";
 import styled from 'styled-components';
 import { useModalContext } from "../../../contexts/ModalContext";
-import THEME from "../../../styles/theme";
-import Icon from "../../shared/ui/Icon";
 
-export const StyledQuickLink = styled.button`
+export const StyledQuickLink = styled.div`
 	height: 100%;
 	padding: 0 16px;
 
-	white-space: nowrap;
-	//cursor: pointer;
-	border: none;
-	text-align: left;
-
 	display: flex;
-	align-items: center;
-	justify-content: space-between;
+	flex-direction: column;
+	align-items: flex-start;
 
-	.name-preview {
-		margin-right: 24px;
+	white-space: nowrap;
+
+	.name-advanced {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		padding: 8px 0;
+		button {
+			background: transparent;
+			&:hover {
+				background: rgba(0,0,0,0.05);
+			}
+			color: ${({ theme }) => theme.clickable};
+			padding: 4px 8px;
+			margin-left: 8px;
+			font-size: 80%;
+		}
 	}
 
 	.name {
+		padding: 4px 0;
 		font-weight: bold;
-		margin-bottom: 2px;
+		font-size: 120%;
 		color: ${({ theme }) => theme.text.dark};
 	}
 
@@ -52,14 +62,20 @@ const QuickLink: React.FC<IQuickLinkProps> = ({ name, children, modalId, closeMe
 
 	return (
 		<StyledQuickLink className="quick-link" type="button">
-			<div className="name-preview">
+			<div className="name-advanced">
 				<div className="name">{name}</div>
-				<div className="content">{children}</div>
+				<ButtonInput onClick={() => {
+					modalContext.setModalId(modalId);
+					closeMenu();
+				}}>
+					Advanced
+				</ButtonInput>
 			</div>
-			<IconButton iconId="edit" color={THEME.text.dark} onClick={() => {
+			<div className="content">{children}</div>
+			{/*<IconButton iconId="edit" color={THEME.text.dark} onClick={() => {
 				modalContext.setModalId(modalId);
 				closeMenu();
-			}} />
+			}} />*/}
 		</StyledQuickLink>
 	);
 };
