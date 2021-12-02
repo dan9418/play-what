5d00d10324461f8072f3c264042d642b9b10e7de
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-12-01 22:04:01 -0800

    improve wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,24 @@
-const React = require("react")
-const { RecoilRoot } = require("recoil");
+import React from "react";
+import { RecoilRoot } from "recoil";
+import { ThemeProvider } from 'styled-components';
+import { ModalContextProvider } from "./src/app/contexts/ModalContext";
+import { RouteContextProvider } from "./src/app/contexts/RouteContext";
+import THEME from "./src/app/styles/theme";
+import ErrorBoundary from "./src/app/components/shared/ErrorBoundary";
 
-exports.wrapRootElement = ({ element }) => {
+export const wrapRootElement = ({ element }) => {
     return (
-        //<ErrorBoundary>
-        <RecoilRoot>
-            {/* <ThemeProvider theme={THEME}>
-                        <ModalContextProvider>
-                            <SiteHeader />
-                            <RouteContextProvider> */}
-            {element}
-            {/* </RouteContextProvider>
-                        </ModalContextProvider>
-                    </ThemeProvider> */}
-        </RecoilRoot>
-        //</ErrorBoundary>
+        <ErrorBoundary>
+            <RecoilRoot>
+                <ThemeProvider theme={THEME}>
+                    <ModalContextProvider>
+                        <RouteContextProvider>
+                            {element}
+                        </RouteContextProvider>
+                    </ModalContextProvider>
+                </ThemeProvider>
+            </RecoilRoot>
+        </ErrorBoundary>
 
     )
 }
