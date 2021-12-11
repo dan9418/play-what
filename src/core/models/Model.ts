d1a84e8fd7d6ed4df94b4f839b172cd7c0e0f9b5
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-12-11 11:20:45 -0800

    comments

--- src/core/models/Model.ts
+++ src/core/models/Model.ts
@@ -20,7 +20,7 @@ export default class Model implements IModelConfig {
 
     // Search
 
-    static isPodAtPitch = (pod: IPod, p: number, matchOctave = false): IPod | null => {
+    /*static isPodAtPitch = (pod: IPod, p: number, matchOctave = false): IPod | null => {
         const pitchOffset = 0;
         const target = p - pitchOffset;
         let a = pod[0];
@@ -30,31 +30,31 @@ export default class Model implements IModelConfig {
             b = NumberUtils.modulo(b, 12);
         }
         return a === b ? pod : null;
-    };
+    };*/
 
-    static getPodAtPitch = (A: IPod[], p: number, matchOctave: boolean): IPod | null => {
+    /*static getPodAtPitch = (A: IPod[], p: number, matchOctave: boolean): IPod | null => {
         const pod = A.find(a => this.isPodAtPitch(a, p, matchOctave));
         return pod ? pod : null;
-    };
+    };*/
 
     // Sound
 
-    static playSound = (note: IPod): void => {
+    /*static playSound = (note: IPod): void => {
         const frequency = TuningUtils.getFrequency(note[0]);
         ToneUtils.playSound(frequency)
-    }
+    }*/
 
-    static playSoundList = (notes: IPod[]): void => {
+    /*static playSoundList = (notes: IPod[]): void => {
         const frequencies = notes.map(n => TuningUtils.getFrequency(n[0]));
         ToneUtils.playSound(frequencies)
-    }
+    }*/
 
     // Search
 
-    static getIndexOfPodAtPitch = (A: IPod[], p: number, matchOctave: boolean): number | null => {
+    /*static getIndexOfPodAtPitch = (A: IPod[], p: number, matchOctave: boolean): number | null => {
         const pod = A.findIndex(a => this.isPodAtPitch(a, p, matchOctave));
         return pod !== -1 ? pod : null;
-    };
+    };*/
 
     // Other
 
@@ -66,7 +66,7 @@ export default class Model implements IModelConfig {
         return true;
     };
 
-    static sort = (podList: IPod[]) => {
+    /*static sort = (podList: IPod[]) => {
         return podList.sort((a, b) => {
             if (a[0] < b[0]) return -1;
             else if (a[0] > b[0]) return 1;
@@ -76,5 +76,5 @@ export default class Model implements IModelConfig {
                 else return 0;
             }
         });
-    }
+    }*/
 }
