30acc01d0e70ba87580efef7d6e50e01237993b7
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-12-18 01:29:47 -0800

    clean up voicing

--- src/components/browse/_shared/cards/GuitarCard.tsx
+++ src/components/browse/_shared/cards/GuitarCard.tsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from 'styled-components';
 import { VOICING_PRESETS } from "../../../../core/theory/Voicing.constants";
 import Fretboard from "../../../../viewers/fretboard/Fretboard";
 import { getFretboardProps } from "../../../../viewers/fretboard/Fretboard.utils";
+import DropdownInput from "../../../_shared/inputs/DropdownInput";
 import Card from "../../../_shared/ui/Card";
 
 const StyledGuitarCard = styled.div`
     
 `;
 
+const OPTIONS = [
+    {
+        id: 'unselected' as any,
+        name: '---',
+        value: undefined
+    },
+    ...VOICING_PRESETS
+]
+
 const GuitarCard: React.FC<any> = ({ model }) => {
 
     if (!model.root || !model.intervals) return null;
 
-    const voicing = VOICING_PRESETS[18];
+    const [voicing, setVoicing] = useState(OPTIONS[0]);
 
-    const fretboardProps = getFretboardProps(model, voicing);
+    const fretboardProps = getFretboardProps(model, voicing.value ? voicing : undefined);
 
     return (
         <Card title="Guitar">
             <StyledGuitarCard>
                 <Fretboard {...fretboardProps} />
+                <div>
+                    <DropdownInput value={voicing} setValue={setVoicing} options={OPTIONS} />
+                </div>
             </StyledGuitarCard>
         </Card >
     );
