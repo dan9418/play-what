0576350b6b4cea0c4063706bb1bf253960a6459a
Dan Bednarczyk <dpbednarczyk@gmail.com>
2021-12-28 09:34:51 -0500

    filtered

--- src/scripts/removeRotations.js
+++ src/scripts/removeRotations.js
@@ -1,37 +1,31 @@
 const fs = require('fs');
 const readline = require('readline');
 
-
 const containsRotation = (arr, val) => {
     const ret = arr.find(line => {
-        const doubleLine = `${(line + line)}`;
-        const doubleVal = `${(val + val)}`;
-        const result = doubleLine.includes(val) && doubleLine !== doubleVal;
+        if (line === val) return false;
+        const doubleLine = `${line + line}`;
+        //const doubleVal = `${(val + val)}`;
+        const result = doubleLine.includes(val);
         //console.log(doubleLine, doubleVal, result);
         return result;
     });
     //console.log('ret', ret);
+    return ret;
 }
 
 async function processLineByLine() {
     const arr = fs.readFileSync('./src/scripts/binary.txt').toString().split(/\r?\n/g);
-    console.log(arr.length)
+    const filtered = [];
 
-    const fileStream = fs.createReadStream('./src/scripts/binary.txt');
-
-    const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-    // Note: we use the crlfDelay option to recognize all instances of CR LF
-    // ('\r\n') in input.txt as a single line break.
-
-    for await (const line of rl) {
-        if (!containsRotation(arr, line.trim())) {
-            //console.log('write');
-            fs.writeFileSync('./src/scripts/filtered.txt', line + '\n', { flag: 'a+' }, err => { })
+    for (let i = 0; i < arr.length; i++) {
+        const line = arr[i];
+        if (!containsRotation(filtered, line)) {
+            filtered.push(line);
         }
     }
+
+    fs.writeFileSync('./src/scripts/filtered.txt', filtered.join('\n'), { flag: 'a+' }, err => { })
 }
 
 processLineByLine();
