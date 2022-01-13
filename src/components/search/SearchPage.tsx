6893682857cb44052b44ee1550a9aebc586d6661
Dan Bednarczyk <dpbednarczyk@gmail.com>
2022-01-13 08:44:40 -0800

    styles

--- src/components/search/SearchPage.tsx
+++ src/components/search/SearchPage.tsx
@@ -8,14 +8,30 @@ const StyledSearchPage = styled(StyledPageBody)`
 	align-items: center;
 	//justify-content: center;
 
-	
+    form {
+        width: 100%;
+        .search-bar {
+            width: 100%;
+            max-width: 512px;
+            margin: auto;
+            display: grid;
+            grid-template-columns: 1fr auto;
+            gap: 8px;
+
+            input, button {
+                padding: 4px 8px;
+                border-radius: 8px;
+                border: 1px solid ${props => props.theme.utils.border};
+            }
+        }
+    }
 `;
 
 const SearchPage: React.FC<any> = () => {
     return (
         <StyledSearchPage>
             <form role="search">
-                <div>
+                <div className="search-bar">
                     <input type="search" id="mySearch" name="q"
                         placeholder="Search the site..."
                         aria-label="Search through site content" />
