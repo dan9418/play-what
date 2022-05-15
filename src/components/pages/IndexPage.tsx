2167473ff30f2a1ce7cef273ffb220dca9375cc0
dan9418 <dpbednarczyk@gmail.com>
2022-05-14 17:35:37 -0700

    improve homepage

--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -2,6 +2,7 @@ import { Link } from "gatsby";
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import logoSrc from '../../../static/logo.png';
+import placeholderSrc from '../../../static/note-logo.png';
 import { getModelIdText } from "../../core/models/Model.constants";
 import { ALL_PRESETS, NOTE_PRESETS } from "../../core/models/Model.presets";
 import { StyledPageBody } from "../layout/PageBody";
@@ -28,6 +29,10 @@ const StyledHomePage = styled(StyledPageBody)`
 		}
 	}
 
+	.placeholder {
+		width: 128px;
+	}
+
 	.intro {
 		max-width: 768px;
 		color: ${({ theme }) => theme.text.primary};
@@ -46,33 +51,52 @@ const StyledHomePage = styled(StyledPageBody)`
 		font-style: italic;
 		color: ${({ theme }) => theme.text.secondary};
 		text-align: center;
-		margin-top: 8px;
+		margin: 8px 0;
 		font-size: 80%;
 		@media(min-width: 512px) {
-			margin-top: 16px;
+			margin: 16px 0;
 			font-size: 100%;
 		}
 		@media(min-width: 1024px) {
-			margin-top: 32px;
+			margin: 32px 0;
 			font-size: 120%;
 		}
 	}
 
-	h3 {
-		margin-top: 64px;
-		font-style: italic;
-		color: ${({ theme }) => theme.surface.nav};
+	section {
+		border-top: 1px solid ${({ theme }) => theme.utils.border};
+		width: 100%;
+    	margin-top: 32px;
+
+		& > div {
+			display: grid;
+			grid-template-columns: 1fr;
+			gap: 32px;
+			@media(min-width: 512px) {
+				grid-template-columns: 1fr 1fr;
+			}
+
+			& > div:first-child {
+				a {
+					display: block;
+					margin-top: 16px;
+				}
+			}
+			& > div:last-child {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+		}
+
+		&:last-child {
+			margin-bottom: 256px;
+		}
 	}
 
-	.search-bar {
-        margin: 8px auto 32px;
-    }
-	
-	.or {
-		font-size: 120%;
-		color: ${({ theme }) => theme.text.secondary};
-		text-align: center;
-		margin-bottom: 32px;
+	h3 {
+		margin: 32px 0 16px;
+		color: ${({ theme }) => theme.surface.nav};
 	}
 
 	> a {
@@ -117,14 +141,56 @@ const IndexPage: React.FC<any> = () => {
 				A toolkit for exploring and visualizing musical concepts
 			</p>
 			<p className="disclaimer">
-				This site is under active development and a formal Beta release is planned for Q2 2022
+				This site is under active development and is currently unstable.<br />A formal Beta release is planned for Q2 2022.
 			</p>
-			<h3>What will you play today?</h3>
-			<SearchBar placeholder={placeholder} />
-			<div className="or">
-				{`~ or ~`}
-			</div>
-			<Link to="/browse">Browse Chords & Scales</Link>
+			<section>
+				<h3>Explore chords and scales</h3>
+				<div>
+					<div>
+						<p>Browse an extensive library of chords, scales, and modes in every key. Easily identify relationships between them and jump between pages.</p>
+						<Link to="/browse">Start Browsing</Link>
+					</div>
+					<div>
+						<img src={placeholderSrc} className="placeholder" />
+					</div>
+				</div>
+			</section>
+			<section>
+				<h3>Visualize notes on instruments</h3>
+				<div>
+					<div>
+						<p>Project notes on a variety of instruments and customize them to your hearts content. Play What supports different tunings, voicings, inversions, and colors.</p>
+						<Link to="/browse">Try It Now</Link>
+					</div>
+					<div>
+						<img src={placeholderSrc} className="placeholder" />
+					</div>
+				</div>
+			</section>
+			<section>
+				<h3>Search the world of music theory</h3>
+				<div>
+					<div>
+						<p>Search for any chord, scale, or mode in any key. Bookmark any page.</p>
+						<Link to="/search">Go To Search Page</Link>
+					</div>
+					<div>
+						<SearchBar placeholder={placeholder} />
+					</div>
+				</div>
+			</section>
+			<section>
+				<h3>Build your own practice materials</h3>
+				<div>
+					<div>
+						<p>Customize your own cheat sheets, flash cards, chord charts, and tabs.</p>
+						<Link to="/">Coming Soon!</Link>
+					</div>
+					<div>
+						<img src={placeholderSrc} className="placeholder" />
+					</div>
+				</div>
+			</section>
 		</StyledHomePage>
 	);
 };
