112f6bf5d2ef152c858b4bc60481aa181d766d75
Dan Bednarczyk <dpbednarczyk@gmail.com>
2022-07-15 18:37:48 -0700

    tabs

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -11,7 +11,7 @@ const StyledModal = styled.div`
 	bottom: 0;
 	right: 0;
 	left: 0;
-	z-index: 10000;
+	z-index: 10000000000000000;
 	display: flex;
 	align-items: center;
 	justify-content: center;
--- src/components/pages/view/TabCard.tsx
+++ src/components/pages/view/TabCard.tsx
@@ -3,20 +3,35 @@ import styled from 'styled-components';
 import { StyledCard } from "../../ui/Card";
 
 const StyledTabCard = styled(StyledCard)`
+    padding: 0;
+    min-height: 256px;
+    
+    .content {
+        padding: 16px;
+    }
+    .fake-border {
+        position: relative;
+        width: 100%;
+        bottom: 2px;
+        border-bottom: 2px solid #EEE;
+        margin-bottom: 16px;
+    }
     > ul {
         display: flex;
         align-items: center;
-        //justify-content: space-between;
-        //border-bottom: 1px solid ${props => props.theme.utils.border};
-        margin-bottom: 16px;
-
+        overflow: auto;
+        
         button {
-            padding: 8px 16px;
+            position: relative;
+            z-index: 100000000;
+            padding: 16px 16px 12px;
             appearance: none;
             border: none;
             cursor: pointer;
             background-color: ${props => props.theme.surface.card};
-            border-bottom: 2px solid ${props => props.theme.utils.border};
+            border-bottom: 2px solid transparent;
+            border-radius: 8px 8px 0 0;
+            border-bottom: 2px solid #EEE;
 
             font-size: 120%;
 
@@ -54,7 +69,8 @@ const TabCard: React.FC<any> = ({ tabs }) => {
                     </li>
                 ))}
             </ul>
-            <div>
+            <div className="fake-border" />
+            <div className="content">
                 {tabs[index].content}
             </div>
         </StyledTabCard>
