ccc06f3b3e5caa6a563d5722e5539c8fbe39c65a
dan9418 <dpbednarczyk@gmail.com>
2022-12-09 16:42:54 -0500

    add page lists

--- src/components/pages/dev/notebook/NotebookPage.tsx
+++ src/components/pages/dev/notebook/NotebookPage.tsx
@@ -8,7 +8,10 @@ import Card, { CardHeader, StyledCard } from "../../../ui/Card";
 const StyledNotebookPage = styled(StyledPageBody)`
     ${StyledCard} {
         margin-top: 16px;
-         li {
+        h3, h4 {
+            margin-top: 16px;
+        }
+        li {
             padding: 8px;
             font-size: 120%;
         }
@@ -21,30 +24,86 @@ const NotebookPage: React.FC<any> = () => {
             <PageTitle title="Music Notebook" subtitle="A work in progress" />
             <Card title="Pages" >
                 <CardHeader as="h3" title="Intervals" />
+                <CardHeader as="h4" title="Basics" />
                 <ul>
+                    <li>Table of intervals and # semitones</li>
+                    <li>Table of intervals from fret positions</li>
+                    <li>Table of intervals mapped to harmonics</li>
+                </ul>
+                <CardHeader as="h4" title="Advanced" />
+                <ul>
+                    <li>Table of extended intervals vs standard intervals</li>
                     <li>
-                        <Link to='/charts'>Chord Charts</Link>
-                    </li>
-                    <li>
-                        <Link to='/voicings'>Chord Voicings</Link>
-                    </li>
-                    <li>
-                        <Link to='/chord-progressions'>Chord Progressions</Link>
-                    </li>
-                    <li>
-                        <Link to='/caged'>CAGED Shapes</Link>
-                    </li>
-                    <li>
-                        <Link to='/extensions'>Extensions</Link>
+                        <Link to='/dev/notebook/extensions'>Possible extensions in basic chords</Link>
                     </li>
                 </ul>
-            </Card>
-            <Card title="Coming Soon" >
+
+                <CardHeader as="h3" title="Chords" subtitle="A work in progress" />
+                <CardHeader as="h4" title="Basics" />
                 <ul>
+                    <li>Comparison of intervals in common chords</li>
+                    <li>Comparison of common chords on fretboard</li>
+                </ul>
+
+                <CardHeader as="h4" title="Voicings" />
+                <ul>
+                    <li>Chord inversions</li>
+                    <li>Triad/seventh voicings from E/A/G/G roots</li>
+                    <li>Shell voicings from E/A/D roots</li>
                     <li>
-                        <Link to='/'>...</Link>
+                        <Link to='/dev/notebook//voicings'>Chord Voicings</Link>
                     </li>
                 </ul>
+
+                <CardHeader as="h4" title="Extended Chords" />
+                <ul>
+                    <li>Table of extensions found/possible in chords</li>
+                    <li>Table of common chords and possible voicings</li>
+                </ul>
+
+                <CardHeader as="h4" title="Altered Chords" />
+                <ul>
+                    <li>List of common examples</li>
+                </ul>
+
+                <CardHeader as="h3" title="Scales" subtitle="A work in progress" />
+                <CardHeader as="h4" title="Basics" />
+                <ul>
+                    <li>Comparison of intervals in common scales</li>
+                    <li>Comparison of common scales on fretboard</li>
+                </ul>
+                <CardHeader as="h4" title="Diatonic Modes" />
+                <ul>
+                    <li>Table of intervals, degrees (#/b), extensions</li>
+                    <li>Table of roman numerals</li>
+                    <li>7 guitar shapes and links</li>
+                </ul>
+
+                <CardHeader as="h3" title="Chord Progressions" subtitle="A work in progress" />
+                <ul>
+                    <li>List of common progressions with roman numerals</li>
+                    <li>
+                        <Link to='/dev/notebook//chord-progressions'>Chord Progressions</Link>
+                    </li>
+                </ul>
+
+                <CardHeader as="h3" title="The CAGED System" subtitle="A work in progress" />
+                <ul>
+                    <li>
+                        <Link to='/dev/notebook//caged'>CAGED Shapes</Link>
+                    </li>
+                    <li>5 shapes</li>
+                    <li>Links between shapes</li>
+                    <li>Chords/scales within shapes</li>
+                </ul>
+
+                <CardHeader as="h3" title="Chord Charts" subtitle="A work in progress" />
+                <ul>
+                    <li>
+                        <Link to='/dev/notebook//charts'>Chord Charts</Link>
+                    </li>
+                </ul>
+
             </Card>
         </StyledNotebookPage >
     )
