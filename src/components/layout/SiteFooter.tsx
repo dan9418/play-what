74a0bf2c96de0aea8bf9cc59754a63f642c19e6f
dan9418 <dpbednarczyk@gmail.com>
2023-01-21 13:38:21 -0500

    0.1.1

--- src/components/layout/SiteFooter.tsx
+++ src/components/layout/SiteFooter.tsx
@@ -1,134 +1,145 @@
-import { Link } from 'gatsby';
-import React from 'react';
-import styled, { useTheme } from 'styled-components';
-import { FEEDBACK_LINK } from '../../config/constants';
+import { Link } from "gatsby";
+import React from "react";
+import styled, { useTheme } from "styled-components";
+import { FEEDBACK_LINK } from "../../config/constants";
 
 const StyledSiteFooter = styled.footer`
-    min-height: 256px;
-	width: 100%;
-	white-space: nowrap;
-	padding: 32px 16px;
-	
-	font-size: 70%;
-	@media(min-width: 512px) {
-		font-size: 80%;
-	}
+  min-height: 256px;
+  width: 100%;
+  white-space: nowrap;
+  padding: 32px 16px;
 
-	color: ${({ theme }) => theme.text.inverted};
-	background-color: ${({ theme }) => theme.surface.nav};
-    box-shadow: 0 2px 10px rgba(0, 0, 0, .3);
+  font-size: 70%;
+  @media (min-width: 512px) {
+    font-size: 80%;
+  }
 
-	h3 {
-		text-transform: uppercase;
-		margin-bottom: 8px;
-		//border-bottom: 1px solid ${({ theme }) => theme.utils.border};
-	}
+  color: ${({ theme }) => theme.text.inverted};
+  background-color: ${({ theme }) => theme.surface.nav};
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 
-	a {
-		display: block;
-		padding: 4px;
-		color: ${({ theme }) => theme.text.inverted};
+  h3 {
+    text-transform: uppercase;
+    margin-bottom: 8px;
+    //border-bottom: 1px solid ${({ theme }) => theme.utils.border};
+  }
 
-		&.name {
-			margin: 32px 0;
-			font-size: 120%;
-			text-align: center;
-		}
-	}
+  a {
+    display: block;
+    padding: 4px;
+    color: ${({ theme }) => theme.text.inverted};
 
-	section {
-		width: 100%;
-		max-width: 160px;
-	}
+    &.name {
+      margin: 32px 0;
+      font-size: 120%;
+      text-align: center;
+    }
+  }
 
-	.nav-links {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		gap: 16px;
-		flex-direction: column;
-		text-align: center;
-		@media(min-width: 512px) {
-			align-items: flex-start;
-			flex-direction: row;
-		}
-	}
+  section {
+    width: 100%;
+    max-width: 160px;
+  }
 
-	.version {
-		margin: 32px 0;
-		font-size: 80%;
-		text-align: center;
-		letter-spacing: 2px;
-	}
+  .nav-links {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    flex-direction: column;
+    text-align: center;
+    @media (min-width: 512px) {
+      align-items: flex-start;
+      flex-direction: row;
+    }
+  }
+
+  .version {
+    margin: 32px 0;
+    font-size: 80%;
+    text-align: center;
+    letter-spacing: 2px;
+  }
 `;
 
 const SiteFooter: React.FC = () => {
-	const theme = useTheme();
-	return (
-		<StyledSiteFooter className="footer">
-			<div className="nav-links">
-				<section>
-					<h3>Site</h3>
-					<ul>
-						<li>
-							<Link to='/'>Home</Link>
-						</li>
-						<li>
-							<Link to='/view'>View</Link>
-						</li>
-						<li>
-							<Link to='/browse'>Browse</Link>
-						</li>
-						<li>
-							<Link to='/search'>Search</Link>
-						</li>
-					</ul>
-				</section>
-				<section>
-					<h3>Resources</h3>
-					<ul>
-						<li>
-							<Link to='/help'>Help</Link>
-						</li>
-						<li>
-							<Link to='/about'>About</Link>
-						</li>
-						<li>
-							<Link to='/coming-soon'>Coming Soon</Link>
-						</li>
-						{/*<li>
+  const theme = useTheme();
+  return (
+    <StyledSiteFooter className="footer">
+      <div className="nav-links">
+        <section>
+          <h3>Site</h3>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/view">View</Link>
+            </li>
+            <li>
+              <Link to="/browse">Browse</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>Resources</h3>
+          <ul>
+            <li>
+              <Link to="/help">Help</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/coming-soon">Coming Soon</Link>
+            </li>
+            {/*<li>
 							<Link to='/coming-soon'>Submit Feedback</Link>
 						</li>*/}
-						<li>
-							<Link to='/dev'>Developer Panel</Link>
-						</li>
-					</ul>
-				</section>
-				<section>
-					<h3>Contact</h3>
-					<ul>
-						<li>
-							<a target="_blank" href='https://github.com/dan9418/play-what'>GitHub</a>
-						</li>
-						<li>
-							<a target="_blank" href='https://www.instagram.com/dan.bednarczyk/'>Instagram</a>
-						</li>
-						<li>
-							<a target="_blank" href='https://danbednarczyk.com'>Author Website</a>
-						</li>
-						<li>
-							<a target="_blank" href={FEEDBACK_LINK}>Provide Feedback</a>
-						</li>
-					</ul>
-				</section>
-			</div>
-			<a href="https://danbednarczyk.com" className="name">
-				2022 Dan Bednarczyk
-			</a>
+            <li>
+              <Link to="/dev">Developer Panel</Link>
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h3>Contact</h3>
+          <ul>
+            <li>
+              <a target="_blank" href="https://github.com/dan9418/play-what">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/dan.bednarczyk/"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href="https://danbednarczyk.com">
+                Author Website
+              </a>
+            </li>
+            <li>
+              <a target="_blank" href={FEEDBACK_LINK}>
+                Provide Feedback
+              </a>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <a href="https://danbednarczyk.com" className="name">
+        2023 Dan Bednarczyk
+      </a>
 
-			<div className='version'>v0.1.0</div>
-		</StyledSiteFooter>
-	);
+      <div className="version">v0.1.1</div>
+    </StyledSiteFooter>
+  );
 };
 
 export default SiteFooter;
