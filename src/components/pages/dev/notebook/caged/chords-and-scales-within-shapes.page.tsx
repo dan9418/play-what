a392ee5815a66c23aeeffe28baff4087e0d2e554
dan9418 <dpbednarczyk@gmail.com>
2023-01-27 18:35:37 -0500

    swap out table

--- src/components/pages/dev/notebook/caged/chords-and-scales-within-shapes.page.tsx
+++ src/components/pages/dev/notebook/caged/chords-and-scales-within-shapes.page.tsx
@@ -5,6 +5,7 @@ import FretTable from "../../../../../viewers/fret-table/FretTable";
 import { VOICING_PRESET_MAP } from "../../../../../viewers/fretboard/Fretboard.voicing";
 import PageLayout from "../../../../layout/PageLayout";
 import Card, { StyledCard } from "../../../../ui/Card";
+import { Table } from "../../../../ui/Table";
 import {
   DIATONIC_ITEMS,
   PENTATONIC_ITEMS,
@@ -34,6 +35,7 @@ const StyledCAGEDPage = styled(PageLayout)`
 
 const DISPLAY = [
   [
+    "Pentatonic",
     PENTATONIC_ITEMS[4],
     PENTATONIC_ITEMS[0],
     PENTATONIC_ITEMS[1],
@@ -41,6 +43,7 @@ const DISPLAY = [
     PENTATONIC_ITEMS[3],
   ],
   [
+    "Diatonic",
     DIATONIC_ITEMS[5],
     DIATONIC_ITEMS[0],
     DIATONIC_ITEMS[1],
@@ -48,6 +51,7 @@ const DISPLAY = [
     DIATONIC_ITEMS[4],
   ],
   [
+    "Seventh",
     ROMAN_NUMERAL_ITEMS[5],
     ROMAN_NUMERAL_ITEMS[0],
     ROMAN_NUMERAL_ITEMS[1],
@@ -69,41 +73,35 @@ const Page: React.FC<any> = () => {
     <StyledCAGEDPage title="CAGED">
       <Card title="Shape Comparisons">
         <div className="scroll">
-          <table className="shapes">
-            <thead></thead>
-            <tbody>
-              {DISPLAY.map((item, i) => {
-                return (
-                  <>
-                    <tr key={i}>
-                      {i === 0 && <th>Pentatonic</th>}
-                      {i === 1 && <th>Diatonic</th>}
-                      {i === 2 && <th>Seventh</th>}
-                      {item.map((item, j) => {
-                        const { model, modelId, rootId, voicingId } =
-                          item as any;
-                        const instance = new model(modelId, {
-                          root: Note.fromId(rootId),
-                        });
-                        return (
-                          <td key={modelId}>
-                            <h3>{instance.getName()}</h3>
-                            <FretTable
-                              model={instance}
-                              voicing={VOICING_PRESET_MAP.get(voicingId)}
-                              fretRange={FRET_RANGE[j]}
-                              showFretNumbers={false}
-                              showFretDots={false}
-                            />
-                          </td>
-                        );
-                      })}
-                    </tr>
-                  </>
-                );
-              })}
-            </tbody>
-          </table>
+          <Table
+            tbody={DISPLAY.map((row, i) => {
+              return {
+                cols: row.map((col, j) => {
+                  if (j === 0) return col;
+                  const { model, modelId, rootId, voicingId } = col as any;
+                  const instance = new model(modelId, {
+                    root: Note.fromId(rootId),
+                  });
+                  return {
+                    content: (
+                      <>
+                        <h3>{instance.getName()}</h3>
+                        <FretTable
+                          model={instance}
+                          voicing={VOICING_PRESET_MAP.get(voicingId)}
+                          fretRange={FRET_RANGE[j - 1]}
+                          showFretNumbers={false}
+                          showFretDots={false}
+                        />
+                      </>
+                    ),
+                  };
+                }),
+              };
+            })}
+            headerColIndicies={[0]}
+            className="shapes"
+          />
         </div>
       </Card>
     </StyledCAGEDPage>
