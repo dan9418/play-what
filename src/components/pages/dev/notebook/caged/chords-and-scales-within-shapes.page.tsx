e61cc6a2e8625ee88c76d95818b8b050fa2fff80
dan9418 <dpbednarczyk@gmail.com>
2023-01-28 11:34:00 -0500

    cleanup

--- src/components/pages/dev/notebook/caged/chords-and-scales-within-shapes.page.tsx
+++ src/components/pages/dev/notebook/caged/chords-and-scales-within-shapes.page.tsx
@@ -30,73 +30,75 @@ const StyledCAGEDPage = styled(PageLayout)`
 const DISPLAY = [
   [
     "Pentatonic",
+    PENTATONIC_ITEMS[2],
+    PENTATONIC_ITEMS[3],
     PENTATONIC_ITEMS[4],
     PENTATONIC_ITEMS[0],
     PENTATONIC_ITEMS[1],
-    PENTATONIC_ITEMS[2],
-    PENTATONIC_ITEMS[3],
   ],
   [
     "Diatonic",
+    DIATONIC_ITEMS[2],
+    DIATONIC_ITEMS[4],
     DIATONIC_ITEMS[5],
     DIATONIC_ITEMS[0],
     DIATONIC_ITEMS[1],
-    DIATONIC_ITEMS[2],
-    DIATONIC_ITEMS[4],
   ],
   [
     "Seventh",
+    ROMAN_NUMERAL_ITEMS[2],
+    ROMAN_NUMERAL_ITEMS[4],
     ROMAN_NUMERAL_ITEMS[5],
     ROMAN_NUMERAL_ITEMS[0],
     ROMAN_NUMERAL_ITEMS[1],
-    ROMAN_NUMERAL_ITEMS[2],
-    ROMAN_NUMERAL_ITEMS[4],
   ],
 ];
 
 const FRET_RANGE: [number, number][] = [
+  [9, 9 + 4],
+  [11, 11 + 4],
   [1, 1 + 4],
   [4, 4 + 4],
   [6, 6 + 4],
-  [9, 9 + 4],
-  [11, 11 + 4],
 ];
+
+const HEAD = [{ cols: ["", "C", "A", "G", "E", "D"] }];
+
+const getRows = (rows: any[]) => {
+  return rows.map((row, i) => {
+    return {
+      cols: row.map((col, j) => {
+        if (j === 0) return col;
+        const { model, modelId, rootId, voicingId } = col as any;
+        const instance = new model(modelId, {
+          root: Note.fromId(rootId),
+        });
+        const voicing = VOICING_PRESET_MAP.get(voicingId);
+        const fretRange = FRET_RANGE[j - 1];
+        return {
+          content: (
+            <FretboardCell
+              caption={instance.getName()}
+              fretboardConfig={{
+                model: instance,
+                voicing,
+                fretRange,
+                showFretNumbers: false,
+                showFretDots: false,
+              }}
+            />
+          ),
+        };
+      }),
+    };
+  });
+};
 
 const Page: React.FC = () => {
   return (
     <StyledCAGEDPage title="CAGED Shapes">
       <Card title="Shape Relationships">
-        <Table
-          thead={[{ cols: ["", "G", "E", "D", "C", "A"] }]}
-          tbody={DISPLAY.map((row, i) => {
-            return {
-              cols: row.map((col, j) => {
-                if (j === 0) return col;
-                const { model, modelId, rootId, voicingId } = col as any;
-                const instance = new model(modelId, {
-                  root: Note.fromId(rootId),
-                });
-                const voicing = VOICING_PRESET_MAP.get(voicingId);
-                const fretRange = FRET_RANGE[j - 1];
-                return {
-                  content: (
-                    <FretboardCell
-                      caption={instance.getName()}
-                      fretboardConfig={{
-                        model: instance,
-                        voicing,
-                        fretRange,
-                        showFretNumbers: false,
-                        showFretDots: false,
-                      }}
-                    />
-                  ),
-                };
-              }),
-            };
-          })}
-          headerColIndicies={[0]}
-        />
+        <Table thead={HEAD} tbody={getRows(DISPLAY)} headerColIndicies={[0]} />
       </Card>
     </StyledCAGEDPage>
   );
