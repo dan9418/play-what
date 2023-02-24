8f824fa74baf86f5a0776a089f5b91b2d3fb0860
dan9418 <dpbednarczyk@gmail.com>
2023-02-23 20:36:28 -0500

    delete file

--- src/core/models/index.ts
+++ src/core/models/index.ts
@@ -61,3 +61,12 @@ export interface IPreset<D extends string, V extends PresetValue> {
 }
 
 export type PresetMap<D extends string, IPreset> = Map<D, IPreset>;
+
+// export const logPresetJson = () => {
+//   console.log("pw-presets notes", JSON.stringify(NOTE_PRESETS));
+//   console.log("pw-presets intervals", JSON.stringify(INTERVAL_PRESETS));
+//   console.log("pw-presets chords", JSON.stringify(CHORD_PRESETS));
+//   console.log("pw-presets scales", JSON.stringify(SCALE_PRESETS));
+// };
+
+// export const POD_LIST_PRESETS = [...SCALE_PRESETS, ...CHORD_PRESETS];
