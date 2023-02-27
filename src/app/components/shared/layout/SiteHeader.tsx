23040ca2e785feffe42101419b6427710f1d662e
dan9418 <dpbednarczyk@gmail.com>
2023-02-26 21:44:26 -0500

    hide unfinished stuff

--- src/app/components/shared/layout/SiteHeader.tsx
+++ src/app/components/shared/layout/SiteHeader.tsx
@@ -3,103 +3,107 @@ import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import SwitchInput from "../inputs/SwitchInput";
-import Icon from "../ui/Icon";
-import Menu from "./Menu";
+//import Icon from "../ui/Icon";
+//import Menu from "./Menu";
 import { useLocation } from "@reach/router";
 import THEME, { DARK_THEME } from "../../../styles/theme";
 import { themeState } from "../../../state";
+import { MediaQuery } from "../../../styles/breakpoint";
 
 const StyledSiteHeader = styled.div`
+  height: 48px;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2000;
+
+  background-color: ${({ theme }) => theme?.surface?.nav};
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+
+  .width-cap {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    width: 100%;
+    //max-width: 1024px;
+    margin: auto;
+  }
+
+  a,
+  .menu {
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+    appearance: none;
+    color: ${({ theme }) => theme?.text?.inverted};
+    text-decoration: none;
+    white-space: nowrap;
     height: 48px;
+    line-height: 48px;
 
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 2000;
+    padding: 0 8px;
 
-	background-color: ${({ theme }) => theme?.surface?.nav};
-	//border-bottom: 1px solid ${(props) => props.theme?.utils?.border};
-    box-shadow: 0 2px 10px rgba(0, 0, 0, .3);
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+      color: white;
+    }
 
-	.width-cap {
-		display: flex;
-		justify-content: space-between;
-		width: 100%;
-		//max-width: 1024px;
-		margin: auto;
-	}
-	
-	a, .menu {
-		background-color: transparent;
-		cursor: pointer;
-    	border: none;
-		appearance: none;
-		color: ${({ theme }) => theme?.text?.inverted};
-		text-decoration: none;
-		white-space: nowrap;
-		height: 48px;
-		line-height: 48px;
+    &.active {
+      border-bottom: 2px solid ${(props) => props.theme?.action?.active};
+    }
+  }
 
-		padding: 0 8px;
+  .logo {
+    font-size: 18px;
+    font-weight: bold;
+    letter-spacing: 2px;
+    ${MediaQuery.Tablet} {
+      font-size: 20px;
+      letter-spacing: 4px;
+    }
+    ${MediaQuery.Desktop} {
+      font-size: 24px;
+    }
+  }
 
-		&:hover {
-			background-color: rgba(255,255,255,0.1);
-			color: white;
-		}
+  .night-mode {
+    display: flex;
+    align-items: center;
+  }
 
-		&.active {
-			border-bottom: 2px solid ${(props) => props.theme?.action?.active};
-		}
-	}
+  .right,
+  .left {
+    display: flex;
+    align-items: center;
+  }
 
-	.logo {
-		font-size: 18px;
-		font-weight: bold;
-		letter-spacing: 2px;
-		@media(min-width: 512px) {
-			font-size: 20px;
-			letter-spacing: 4px;
-		}
-		@media(min-width: 1024px) {
-			font-size: 24px;
-		}
-	}
+  .right {
+    margin-right: 16px;
+    gap: 8px;
 
-	.night-mode {
-		display: flex;
-		align-items: center;
-	}
+    a {
+      display: none;
+    }
+    ${MediaQuery.Tablet} {
+      a {
+        display: flex;
+      }
+    }
+  }
 
-	.right, .left {
-		display: flex;
-		align-items; center;
-	}
-	
-	.right {
-		margin-right: 16px;
-		gap: 8px;
+  .icon,
+  .menu {
+    width: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-		a {
-			display: none;
-		}
-		@media(min-width: 512px) {
-			a {
-				display: flex;
-			}
-		}
-	}
-
-	.icon, .menu {
-		width: 48px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.icon {
-		width: 32px;
-	}
+  .icon {
+    width: 32px;
+  }
 `;
 
 const SiteHeader: React.FC = () => {
@@ -118,13 +122,13 @@ const SiteHeader: React.FC = () => {
       <StyledSiteHeader>
         <div className="width-cap">
           <div className="left">
-            <button
+            {/*<button
               type="button"
               className="menu"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               <Icon iconId="menu"></Icon>
-            </button>
+  			</button>*/}
             {pathname !== "/" && (
               <Link to="/" className="logo">
                 Play What?
@@ -132,19 +136,19 @@ const SiteHeader: React.FC = () => {
             )}
           </div>
           <div className="right">
-            <Link to="/help" className="icon" title="Help">
+            {/*<Link to="/help" className="icon" title="Help">
               <Icon iconId="help" />
             </Link>
             <Link to="/about" className="icon" title="About">
               <Icon iconId="info" />
-            </Link>
+			</Link>*/}
             <div className="night-mode">
               <SwitchInput value={isNightMode} setValue={onThemeToggle} />
             </div>
           </div>
         </div>
       </StyledSiteHeader>
-      <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />
+      {/*<Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} />*/}
     </>
   );
 };
