3380e262b7d4f74fff57570812e2095473c07cd3
dan9418 <dpbednarczyk@gmail.com>
2023-02-26 19:41:43 -0500

    strong type gatsby config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,12 +1,12 @@
-const NOTE_JSON = require("./static/notes.json")
-const CHORD_JSON = require("./static/chords.json")
-const SCALE_JSON = require("./static/scales.json")
+const NOTE_JSON = require("./static/notes.json");
+const CHORD_JSON = require("./static/chords.json");
+const SCALE_JSON = require("./static/scales.json");
 
 const STATIC_PAGES = [
-  { route: '/index/' },
+  { route: "/index/" },
   //{ route: '/browse/index/' },
- // { route: '/browse/chords/' },
- // { route: '/browse/scales/' },
+  // { route: '/browse/chords/' },
+  // { route: '/browse/scales/' },
   //{ route: '/about/' },
   //{ route: '/coming-soon/' },
   //{ route: '/help/' },
@@ -68,14 +68,17 @@ const STATIC_PAGES = [
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  STATIC_PAGES.forEach(page => {
+  STATIC_PAGES.forEach((page) => {
     const { route } = page;
-    const path = route.replace('/index/', '/');
-    const componentPath = `./src/app/components/pages${route.slice(0, route.length - 1)}.page.tsx`;
+    const path = route.replace("/index/", "/");
+    const componentPath = `./src/app/components/pages${route.slice(
+      0,
+      route.length - 1
+    )}.page.tsx`;
     createPage({
       path,
       component: require.resolve(componentPath),
-      context: {}
+      context: {},
     });
   });
 
@@ -112,5 +115,4 @@ module.exports.createPages = async ({ graphql, actions }) => {
   //   })
 
   // });
-
-}
+};
--- gatsby-shared.tsx
+++ gatsby-shared.tsx
@@ -5,29 +5,27 @@ import SiteFooter from "./src/app/components/shared/layout/SiteFooter";
 import SiteHeader from "./src/app/components/shared/layout/SiteHeader";
 import ErrorBoundary from "./src/app/components/shared/utils/ErrorBoundary";
 import { PagePropsContextProvider } from "./src/app/components/shared/utils/PagePropsContext";
-import './src/app/styles/global.css';
+import "./src/app/styles/global.css";
 import { ThemeManager } from "./src/app/styles/ThemeManager";
 
 export const wrapRootElement = ({ element, ...props }) => {
-    return (
-        <ErrorBoundary>
-            <RecoilRoot>
-                <ThemeManager>
-                    {element}
-                </ThemeManager>
-            </RecoilRoot>
-        </ErrorBoundary>
-    )
-}
+  return (
+    <ErrorBoundary>
+      <RecoilRoot>
+        <ThemeManager>{element}</ThemeManager>
+      </RecoilRoot>
+    </ErrorBoundary>
+  );
+};
 
 export const wrapPageElement = ({ element, props }) => {
-    return (
-        <PagePropsContextProvider value={props}>
-            <SiteHeader />
-            <Main>
-                {element}
-                <SiteFooter />
-            </Main>
-        </PagePropsContextProvider>
-    );
+  return (
+    <PagePropsContextProvider value={props}>
+      <SiteHeader />
+      <Main>
+        {element}
+        <SiteFooter />
+      </Main>
+    </PagePropsContextProvider>
+  );
 };
