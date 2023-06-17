460ab847bbe6f085871a86c248d93db86496c9c2
Dan Bednarczyk <dpbednarczyk@gmail.com>
2023-06-17 08:50:52 -0400

    finish details card

--- src/app/components/pages/browse/cards/DetailsCard.tsx
+++ src/app/components/pages/browse/cards/DetailsCard.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import { AnyPodListPreset } from "../../../../../core/Core.derived";
 import { IIntervalPreset, INTERVAL_PRESET_MAP, IntervalId } from "../../../../../core/Interval.constants";
-import { getName } from "../../../../../core/Interval.utils";
-import { INotePreset, NOTE_PRESET_MAP, NoteId } from "../../../../../core/Note.constants";
-import { getName as getNoteName } from "../../../../../core/Note.utils";
+import { getName as getIntervalName, getRatio } from "../../../../../core/Interval.utils";
+import { INotePreset } from "../../../../../core/Note.constants";
+import { getFrequency, getName as getNoteName } from "../../../../../core/Note.utils";
 import { addPods } from "../../../../../core/Pod.utils";
 import RootInput from "../../../shared/inputs/RootInput";
 import { StyledCard } from "../../../shared/ui/Card";
 
 const StyledDetailsCard = styled(StyledCard) <{ $n: number }>`
     ul {
-        font-weight: bold;
         display: flex;
         align-items: center;
         justify-content: space-evenly;
     }
-    .notes {
-        font-size: 150%;
-    }
-    .intervals {
-        font-size: 120%;
+    li {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 8px;
+        >:first-child {
+            font-weight: bold;
+            font-size: 150%;
+        }
+        >:nth-child(2) {
+            font-weight: semibold;
+            font-size: 120%;
+        }
     }
 `;
 
@@ -33,35 +40,30 @@ const DetailsCard: React.FC<IDetailsCardProps> = ({
     podListPreset,
     rootNotePreset,
 }) => {
-    console.log('dpb', podListPreset, rootNotePreset);
-
+    const hasRoot = !!rootNotePreset;
     const intervalIds = podListPreset.valueIds;
-
-    const noteNames = !rootNotePreset
-        ? undefined
-        : podListPreset.pods.map(pod => {
-            return getNoteName(addPods(rootNotePreset.pod, pod))
-        })
 
     return (
         <StyledDetailsCard $n={intervalIds.length}>
-            <ul className="notes">
-                {noteNames && noteNames.map(n => {
-                    //const preset = NOTE_PRESET_MAP.get(noteId as NoteId) as INotePreset;
-                    return (
-                        <li key={n}>{n}</li>
-                    );
-                })}
-            </ul>
             <ul className="intervals">
                 {intervalIds.map(ivlId => {
-                    const preset = INTERVAL_PRESET_MAP.get(ivlId as IntervalId) as IIntervalPreset;
+                    const intervalPreset = INTERVAL_PRESET_MAP.get(ivlId as IntervalId) as IIntervalPreset;
+                    const intervalPod = intervalPreset.pod;
+                    const intervalName = getIntervalName(intervalPod)
+                    const notePod = rootNotePreset && addPods(rootNotePreset.pod, intervalPreset.pod);
+                    const noteName = notePod && getNoteName(notePod);
+                    const ratio = getRatio(intervalPod);
+                    const freq = notePod && getFrequency(notePod);
                     return (
-                        <li key={ivlId}>{getName(preset.pod)}</li>
+                        <li key={intervalName}>
+                            {noteName && <div className="note">{noteName}</div>}
+                            <div className="interval">{intervalName}</div>
+                            {freq && <div className="freq">{freq} Hz</div>}
+                            <div className="ratio">{ratio}</div>
+                        </li>
                     );
                 })}
             </ul>
-
             <RootInput />
         </StyledDetailsCard>
     );
